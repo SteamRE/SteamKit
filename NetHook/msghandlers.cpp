0d0b010f8a2e4211bb940751087abd4f85de4071
Ryan Stecker <voidedweasel@gmail.com>
2010-08-12 22:10:09 +0000

    --HG-- extra : convert_revision : svn%3A946a0da7-ebce-4904-9acb-2f1e67aed693%4014

--- NetHook/msghandlers.cpp
+++ NetHook/msghandlers.cpp
@@ -3,7 +3,15 @@
 
 #include "logger.h"
 
+
+struct MsgClientAnonLogOn_t
+{
+
+};
+
 DEFINE_MSGHANDLER( MsgClientAnonLogOn,
+
+	// todo
+	ExtendedClientMsgHdr_t *extHdr = (ExtendedClientMsgHdr_t *)pData;
 	return true;
 );
-
--- NetHook/msgmanager.cpp
+++ NetHook/msgmanager.cpp
@@ -1,13 +1,17 @@
 
 #include "msgmanager.h"
 
+#include "logger.h"
+#include "utils.h"
+
 
 CMsgManager *g_pMsgManager = NULL;
+
+const char CMsgManager::m_szLogFile[] = "EMsgLog Detailed.txt";
 
 CMsgManager::CMsgManager() :
 	m_Handlers( DefLessFunc( EMsg ) )
 {
-
 }
 
 
@@ -36,6 +40,19 @@ void CMsgManager::Unregister( EMsg eMsg )
 
 bool CMsgManager::HandleMsg( EMsg eMsg, ENetDirection eDirection, const uint8 *pData, uint32 cubData )
 {
+	// do our own logging
+	g_Logger->AppendFile( m_szLogFile, "%s %s EMsg: %s ( %s)\r\n",
+		NET_ARROW_STRING( eDirection ),
+		NET_DIRECTION_STRING( eDirection ),
+		PchNameFromEMsg( eMsg ),
+		PchStringFromData( pData, 4 )
+	);
+
+	g_Logger->AppendFile( m_szLogFile, "    %s\r\n\r\n", PchStringFromData( pData, cubData ) );
+
+
+
+	// check against registered handlers
 	MsgMapIndex indx = m_Handlers.Find( eMsg );
 
 	if ( indx == m_Handlers.InvalidIndex() )
@@ -43,5 +60,5 @@ bool CMsgManager::HandleMsg( EMsg eMsg, ENetDirection eDirection, const uint8 *p
 
 	Assert( m_Handlers[ indx ] );
 
-	return m_Handlers[ indx ]->HandleMsg( eDirection, pData, cubData );
+	return m_Handlers[ indx ]->HandleMsg( eMsg, eDirection, pData, cubData );
 }
