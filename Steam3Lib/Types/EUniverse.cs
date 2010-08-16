4740ee11c70ab1ee432475f6ecba31145073be52
Ryan Stecker <voidedweasel@gmail.com>
2010-08-16 09:22:15 +0000

    --HG-- extra : convert_revision : svn%3A946a0da7-ebce-4904-9acb-2f1e67aed693%4030

---
+++ Steam3Lib/Types/EUniverse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SteamLib
+{
+    public enum EUniverse : int
+    {
+        Invalid = 0,
+
+        Public = 1,
+        Beta = 2,
+        Internal = 3,
+        Dev = 4,
+        RC = 5,
+
+        Max
+    }
+
+
+}
