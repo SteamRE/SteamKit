96d4746cd234a261c47b7b91af45606994ddb29f
Ryan Stecker <voidedweasel@gmail.com>
2010-08-21 13:22:51 +0000

    --HG-- extra : convert_revision : svn%3A946a0da7-ebce-4904-9acb-2f1e67aed693%4044

---
+++ Steam3Lib/Steam2/Client.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+
+namespace SteamLib
+{
+    public enum EServerType : uint
+    {
+        ProxyASClientAuthentication = 0,
+        ContentServer = 1,
+        GeneralDirectoryServer = 2,
+        ConfigServer = 3,
+        CSDS = 6,
+        VCDSValidateNewValveCDKey = 7,
+        HLMasterServer = 15,
+        FriendsServer = 16,
+        AllMasterASClientAuthentication = 18,
+        CSER = 20,
+        HL2MasterServer = 24,
+        MasterASClientAuthentication = 26,
+        SlaveASClientAuthentication = 28,
+    }
+
+    public class ServerClient
+    {
+
+        public TcpSocket socket;
+
+
+        public ServerClient()
+        {
+            socket = new TcpSocket();
+        }
+
+
+        public bool ConnectToServer( IPEndPoint endPoint )
+        {
+            try
+            {
+                socket.Connect( endPoint );
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool HandshakeServer( EServerType type )
+        {
+            try
+            {
+                socket.Writer.Write( ( uint )type );
+                return socket.Reader.ReadByte() == 1;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool SendCommand( byte command, params object[] args )
+        {
+            try
+            {
+                TcpPacket packet = new TcpPacket();
+
+                packet.Append( command );
+
+                foreach ( object arg in args )
+                {
+                    if ( arg is byte[] )
+                        packet.Append( ( byte[] )arg );
+                    else if ( arg is string )
+                        packet.Append( ( string )arg, Encoding.ASCII );
+                    else
+                        packet.Append( arg.GetType(), arg );
+                }
+
+                socket.Send( packet );
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public TcpPacket RecvPacket()
+        {
+            try
+            {
+                return socket.Receive();
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public void Disconnect()
+        {
+            try
+            {
+                socket.Disconnect();
+            }
+            catch { }
+        }
+    }
+}
---
+++ Steam3Lib/Steam2/GeneralDirectoryServerClient.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SteamLib
+{
+    public class GeneralDSClient : DSClient
+    {
+        public static string[] GDServers =
+        {
+            "72.165.61.189:27030", // gds1.steampowered.com
+            "72.165.61.190:27030", // gds2.steampowered.com
+            "69.28.151.178:27038",
+            "69.28.153.82:27038",
+            "87.248.196.194:27038",
+            "68.142.72.250:27038",
+        };
+    }
+}
