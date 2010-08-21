7cd71d744cb73c0fba019b1e0288341a3ddb03fe
Ryan Stecker <voidedweasel@gmail.com>
2010-08-21 23:15:54 +0000

    --HG-- extra : convert_revision : svn%3A946a0da7-ebce-4904-9acb-2f1e67aed693%4054

--- Steam3Tester/Program.cs
+++ Steam3Tester/Program.cs
@@ -2,274 +2,57 @@
 using System.Collections.Generic;
 using System.Text;
 using SteamLib;
-using System.Net;
-using System.IO;
 using System.Runtime.InteropServices;
-using System.IO.Compression;
-using System.Net.Sockets;
-using System.Threading;
 using System.Diagnostics;
+using System.Net;
+using System.Threading;
 
 namespace Steam3Tester
 {
-    enum ECacheState : byte
-    {
-        eCacheEmpty,
-        eCachedMallocedPreprocessedVersion,
-        eCachedMallocedPlaintextVersion,
-        eCachePtrIsCopyOnWritePreprocessedVersion,
-        eCachePtrIsCopyOnWritePlaintextVersion
-    }
-    enum EAutoPreprocessCode : byte
-    {
-        eAutoPreprocessCodePlaintext = ( byte )'P', // P = Plaintext
-        eAutoPreprocessCodeCompressed = ( byte )'C', // C = Compressed
-        eAutoPreprocessCodeEncrypted = ( byte )'E', // E = Encrypted
-    }
-
-    [StructLayout( LayoutKind.Sequential, Pack = 1 )]
-    class TBlobHeader : Serializable<TBlobHeader>
-    {
-        public ECacheState CacheState;
-        public EAutoPreprocessCode AutoPreprocessCode;
-        public uint SizeOfSerializedBlob;
-        public uint SizeOfSpareCapacity;
-    }
-
-    [StructLayout( LayoutKind.Sequential, Pack = 1 )]
-    class TCompressedBlobHeader : Serializable<TCompressedBlobHeader>
-    {
-        public TBlobHeader Header;
-        public uint DecompressedSize;
-        public uint Unknown;
-        public ushort CompressionLevel;
-    }
-
-    class ServerList
-    {
-        List<IPEndPoint> servers;
-
-        public EServerType Type { get; private set; }
-
-        public ServerList( EServerType type )
-        {
-            this.Type = type;
-            this.servers = new List<IPEndPoint>();
-        }
-
-        public void AddServer( IPEndPoint endPoint )
-        {
-            foreach ( IPEndPoint server in servers )
-            {
-                if ( server.Address.Equals( endPoint.Address ) && server.Port == endPoint.Port )
-                    return;
-            }
-
-            servers.Add( endPoint );
-        }
-
-        public IPEndPoint[] GetServers()
-        {
-            return servers.ToArray();
-        }
-    }
-
-    static class ServerCache
-    {
-        static Dictionary<EServerType, ServerList> serverMap;
-
-        static ServerCache()
-        {
-            serverMap = new Dictionary<EServerType, ServerList>();
-        }
-
-        public static void AddServer( EServerType type, IPEndPoint server )
-        {
-            if ( !serverMap.ContainsKey( type ) )
-                serverMap.Add( type, new ServerList( type ) );
-
-            serverMap[ type ].AddServer( server );
-        }
-
-        public static void AddServers( EServerType type, IPEndPoint[] servers )
-        {
-            if ( servers == null )
-                return;
-
-            foreach ( IPEndPoint endPoint in servers )
-                AddServer( type, endPoint );
-        }
-
-        public static ServerList GetServers( EServerType type )
-        {
-            if ( !serverMap.ContainsKey( type ) )
-                return null;
-
-            return serverMap[ type ];
-        }
-
-        public static ServerList[] GetServerLists()
-        {
-            List<ServerList> serverLists = new List<ServerList>();
-
-            foreach ( var kvp in serverMap )
-                serverLists.Add( kvp.Value );
-
-            return serverLists.ToArray();
-        }
-
-
-    }
-
-
     class Program
     {
-
-        static IPEndPoint GetAddress( string server )
-        {
-            string[] split = server.Split( ':' );
-
-            IPAddress ipAddr = IPAddress.Parse( split[ 0 ] );
-            int port = int.Parse( split[ 1 ] );
-
-            IPEndPoint endPoint = new IPEndPoint( ipAddr, port );
-
-            return endPoint;
-        }
-
-        static byte[] GenerateAESKey( uint salt1, uint salt2, string pass )
-        {
-            ByteBuffer bb = new ByteBuffer();
-
-            bb.Append( salt1 );
-            bb.Append( Encoding.ASCII.GetBytes( pass ) );
-            bb.Append( salt2 );
-
-            byte[] digest = CryptoHelper.SHAHash( bb.ToArray() );
-
-            byte[] aesKey = new byte[ 16 ];
-            Array.Copy( digest, 0, aesKey, 0, 16 );
-
-            return aesKey;
-        }
-
-        static ulong GetObfuscationMask( uint internalIp, uint externalIp )
-        {
-            ByteBuffer bb = new ByteBuffer();
-
-            bb.Append( externalIp );
-            bb.Append( internalIp );
-        
-            byte[] digest = CryptoHelper.SHAHash( bb.ToArray() );
-
-            return BitConverter.ToUInt64( digest, 0 );
-        }
-
-
-        static uint ToUnixTime( DateTime dt )
-        {
-            TimeSpan ts = ( dt - new DateTime( 1970, 1, 1, 0, 0, 0 ) );
-            return ( uint )ts.TotalSeconds;
-        }
-
-
-        static ulong GetMicroseconds()
-        {
-            return 0xDCBFFEFF2BC000UL + ( ( ulong )ToUnixTime( DateTime.UtcNow ) * 1000000UL );
-        }
-
-        static byte[] GetPlaintext( ulong timeStamp, uint internalIp )
-        {
-            ByteBuffer bb = new ByteBuffer();
-
-            bb.Append( timeStamp );
-            bb.Append( internalIp );
-
-            return bb.ToArray();
-        }
-
         static void Main( string[] args )
         {
-            byte[] intIpData = Dns.GetHostByName( Dns.GetHostName() ).AddressList[ 0 ].GetAddressBytes();
-            uint internalIp = BitConverter.ToUInt32( intIpData, 0 );
+            SteamError err;
+            SteamCallHandle loginHandle = Steam2.Login( "username", "password", out err );
 
-            string userName = ""; // fill this in
-            string password = ""; // fill this in
+            if ( err.IsError() )
+            {
+                Console.WriteLine( "Error: {0}", err.ToString() );
+                Console.ReadKey();
+
+                return;
+            }
+
+            SteamProgress progress = new SteamProgress();
+            while ( !Steam2.ProcessCall( loginHandle, ref progress, out err ) )
+            {
+                Console.WriteLine( "Progress: {0}", progress.Description );
+
+                Thread.Sleep( 10 );
+            }
+
+            if ( err.IsError() )
+            {
+                Console.WriteLine( "Error: {0}", err.ToString() );
+                Console.ReadKey();
+
+                return;
+            }
+
+            SteamGlobalUserID userId;
+            if ( !Steam2.GetUserID( out userId, out err ) )
+            {
+                Console.WriteLine( "Error: {0}", err.ToString() );
+                Console.ReadKey();
+
+                return;
+            }
+
+            Console.WriteLine( "UserID: {0}", userId.AccountID );
 
 
-            ServerClient client = new ServerClient();
 
-            client.ConnectToServer( new IPEndPoint( IPAddress.Parse( "72.165.61.139" ), 27039 ) );
-
-
-            // send protocol and ip request packet
-            ByteBuffer bb = new ByteBuffer( true );
-
-            bb.Append( ( uint )0 );
-            bb.Append( ( byte )4 );
-            bb.Append( internalIp );
-            bb.Append( ( uint )0 );
-
-            client.socket.Writer.Write( bb.ToArray() );
-
-
-            byte result = client.socket.Reader.ReadByte();
-            uint externalIp = ( uint )IPAddress.NetworkToHostOrder( client.socket.Reader.ReadInt32() );
-
-            client.SendCommand( 2, ( ushort )userName.Length, userName, ( ushort )userName.Length, userName );
-
-            uint salt1 = client.socket.Reader.ReadUInt32();
-            uint salt2 = client.socket.Reader.ReadUInt32();
-
-            byte[] aesKey = GenerateAESKey( salt1, salt2, password );
-            byte[] iv = CryptoHelper.GenerateRandomBlock( 16 );
-
-            // time data
-            ulong microseconds = GetMicroseconds() ^ GetObfuscationMask( internalIp, externalIp );
-
-            byte[] plainText = GetPlaintext( microseconds, internalIp );
-            byte[] cipherText = CryptoHelper.AESEncrypt( plainText, aesKey, iv );
-
-            // reply data
-            TcpPacket packet = new TcpPacket();
-            packet.Append( iv );
-            packet.Append( ( ushort )plainText.Length );
-            packet.Append( ( ushort )cipherText.Length );
-            packet.Append( cipherText );
-
-            client.socket.Send( packet );
- 
-            result = client.socket.Reader.ReadByte();
-            ulong loginTime = client.socket.Reader.ReadUInt64();
-            ulong unk = client.socket.Reader.ReadUInt64();
-
-            packet = client.RecvPacket();
-
-            DataStream ds = new DataStream( packet.GetPayload(), true );
-
-            // client tgt
-            ushort versionNum = ds.ReadUInt16();
-            byte[] tgtIv = ds.ReadBytes( 16 );
-
-            ushort tgtPlaintextSize = ds.ReadUInt16();
-            ushort tgtCiphertextSize = ds.ReadUInt16();
-
-            byte[] tgtEncrypted = ds.ReadBytes( tgtCiphertextSize );
-            byte[] tgtPlaintext = CryptoHelper.AESDecrypt( tgtEncrypted, aesKey, tgtIv );
-
-            ClientTGT tgt = ClientTGT.Deserialize( tgtPlaintext );
-
-            // server tgt
-            ushort serverTGTSize = ds.ReadUInt16();
-            byte[] serverTGT = ds.ReadBytes( serverTGTSize );
-
-            File.WriteAllBytes( "tgt_server.bin", serverTGT );
-
-            // account record: encrypted CMultiFieldBlob
-            uint accRecordSize = ds.ReadUInt32();
-            byte[] accRecord = ds.ReadBytes( ds.SizeRemaining() );
-
-            File.WriteAllBytes( "acc_record.bin", accRecord );
 
 
 #if false
@@ -379,11 +162,6 @@ namespace Steam3Tester
                 File.AppendAllText( "servers.txt", "\r\n" );
             }
 
-
-            //CMInterface cmInterface = new CMInterface();
-
-            //cmInterface.ConnectToCM();
-
             
             GDSClient gdsClient = new GDSClient();
             List<IPEndPoint> fullCSDSList = new List<IPEndPoint>();
@@ -430,24 +208,6 @@ namespace Steam3Tester
             }
 #endif
 
-        }
-
-
-        [StructLayout( LayoutKind.Sequential, Pack = 1 )]
-        class ClientTGT : Serializable<ClientTGT>
-        {
-            [MarshalAs( UnmanagedType.ByValArray, SizeConst = 16 )]
-            public byte[] AccountRecordKey;
-
-            public ushort Unknown;
-
-            public ulong SteamGlobalUserID;
-
-            public IPAddrPort Server1;
-            public IPAddrPort Server2;
-
-            public MicroTime CreationTime;
-            public MicroTime ExpirationTime;
         }
     }
 }
