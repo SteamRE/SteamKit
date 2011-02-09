de86237049fae44f304a178af97fccec1583ed44
high6 <high828@gmail.com>
2011-02-09 09:00:30 +0000

    Tidied up the NetLoop
    --HG--
    extra : convert_revision : svn%3A946a0da7-ebce-4904-9acb-2f1e67aed693%40154

--- SteamKit2/SteamKit2/Networking/Steam3/TcpConnection.cs
+++ SteamKit2/SteamKit2/Networking/Steam3/TcpConnection.cs
@@ -16,145 +16,104 @@ using System.Threading;
 
 namespace SteamKit2
 {
-    
+
     class TcpConnection : Connection
     {
         const uint MAGIC = 0x31305456;
 
         Socket sock;
-        MemoryStream recvBuffer;
 
         Thread netThread;
-        bool bConnected;
 
-        int sizeLeft;
+        NetworkStream stream;
+        BinaryReader reader;
+        BinaryWriter writer;
 
-
-        public TcpConnection()
+        public override void Connect(IPEndPoint endPoint)
         {
-            sock = new Socket( AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp );
+            Disconnect();
 
-            recvBuffer = new MemoryStream();
+            sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            sock.Connect(endPoint);
 
-            bConnected = false;
+            stream = new NetworkStream(sock, true);
+            reader = new BinaryReader(stream);
+            writer = new BinaryWriter(stream);
 
-
-        }
-
-
-        public override void Connect( IPEndPoint endPoint )
-        {
-            sock.Connect( endPoint );
-
-            bConnected = true;
-
-            netThread = new Thread( NetLoop );
+            netThread = new Thread(NetLoop);
             netThread.Start();
         }
 
         public override void Disconnect()
         {
-            if ( !sock.Connected )
+            if (sock == null || !sock.Connected)
                 return;
 
-            sock.Shutdown( SocketShutdown.Both );
-            sock.Disconnect( true );
-            sock.Close();
-
-            lock ( ConnLock )
+            if (sock != null)
             {
-                bConnected = false;
-            }
+                try
+                {
+                    sock.Shutdown(SocketShutdown.Both);
+                    sock.Disconnect(true);
+                    sock.Close();
 
-            netThread.Join();
+                    sock = null;
+                }
+                catch { }
+            }
         }
 
-        public override void Send( IClientMsg clientMsg )
+        public override void Send(IClientMsg clientMsg)
         {
-            if ( !sock.Connected )
+            if (!sock.Connected)
                 return;
 
             byte[] data = clientMsg.Serialize();
 
-            if ( NetFilter != null )
+            if (NetFilter != null)
             {
-                data = NetFilter.ProcessOutgoing( data );
+                data = NetFilter.ProcessOutgoing(data);
             }
 
-            ByteBuffer bb = new ByteBuffer( data.Length + 8 );
-            bb.Append( ( uint )data.Length );
-            bb.Append( TcpConnection.MAGIC );
-            bb.Append( data );
+            ByteBuffer bb = new ByteBuffer(data.Length + 8);
+            bb.Append((uint)data.Length);
+            bb.Append(TcpConnection.MAGIC);
+            bb.Append(data);
 
-            sock.Send( bb.ToArray() );
+            writer.Write(bb.ToArray());
         }
 
         void NetLoop()
         {
-            while ( true )
+            try
             {
-                // todo: determine if we have any real performance issues here
-                Thread.Sleep( 10 );
-
-                lock ( ConnLock )
+                while (sock.Connected)
                 {
-                    if ( !bConnected )
-                        return;
-                }
+                    byte[] packetHeader = reader.ReadBytes(8);
 
-                if ( sock.Available == 0 )
-                    continue; // we don't want to stay blocked incase the thread needs to shutdown
-                
-                if ( sizeLeft == 0 )
-                {
-                    // new packet
-                    recvBuffer.SetLength( 0 );
+                    if (packetHeader.Length != 8)
+                        throw new IOException("Connection lost");
 
-                    byte[] packetHeader = new byte[ 8 ];
-                    int headerLen = sock.Receive( packetHeader );
-
-                    if ( headerLen != 8 )
-                    {
-                        DebugLog.WriteLine( "TcpConnection", "Recv'd truncated packet header!!" );
-                        continue;
-                    }
-
-                    DataStream ds = new DataStream( packetHeader );
-
+                    DataStream ds = new DataStream(packetHeader);
                     uint packetLen = ds.ReadUInt32();
                     uint packetMagic = ds.ReadUInt32();
 
-                    if ( packetMagic != TcpConnection.MAGIC )
-                    {
-                        DebugLog.WriteLine( "TcpConnection", "RecvCompleted got a packet with invalid magic!" );
-                        return;
-                    }
+                    if (packetMagic != TcpConnection.MAGIC)
+                        throw new IOException("RecvCompleted got a packet with invalid magic!");
 
-                    sizeLeft = ( int )packetLen;
+                    byte[] packData = reader.ReadBytes((int)packetLen);
+                    if (packData.Length != packetLen)
+                        throw new IOException("Connection lost");
+
+                    if (NetFilter != null)
+                        packData = NetFilter.ProcessIncoming(packData);
+
+                    OnNetMsgReceived(new NetMsgEventArgs(packData));
                 }
-
-                // continuing packet
-                int readSize = Math.Min( 1024, sizeLeft );
-                byte[] packetChunk = new byte[ readSize ];
-
-                int numRead = sock.Receive( packetChunk );
-
-                recvBuffer.Write( packetChunk, 0, numRead );
-                sizeLeft -= numRead;
-
-                if ( sizeLeft == 0 )
-                {
-                    // packet completed
-                    byte[] packData = recvBuffer.ToArray();
-
-                    if ( NetFilter != null )
-                    {
-                        packData = NetFilter.ProcessIncoming( packData );
-                    }
-
-                    OnNetMsgReceived( new NetMsgEventArgs( packData ) );
-                }
-
+            }
+            catch (Exception e)
+            {
+                DebugLog.WriteLine("TcpConnection", e.ToString());
             }
         }
 
