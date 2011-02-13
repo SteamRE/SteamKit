7dac527673050600e44634a9c34156a5ca935614
Ryan Stecker <voidedweasel@gmail.com>
2011-02-13 06:44:26 +0000

    Added WaitForCallback functions.
    --HG--
    extra : convert_revision : svn%3A946a0da7-ebce-4904-9acb-2f1e67aed693%40173

--- SteamKit2/SteamKit2/Steam3/SteamClient/SteamClient.cs
+++ SteamKit2/SteamKit2/Steam3/SteamClient/SteamClient.cs
@@ -9,6 +9,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 
 namespace SteamKit2
 {
@@ -37,6 +38,8 @@ namespace SteamKit2
             this.AddHandler( new SteamFriends() );
         }
 
+
+        #region Handlers
         /// <summary>
         /// Adds a new handler to the internal list of message handlers.
         /// </summary>
@@ -52,6 +55,7 @@ namespace SteamKit2
             handlers[ handler.Name ] = handler;
             handler.Setup( this );
         }
+
         /// <summary>
         /// Removes a registered handler by name.
         /// </summary>
@@ -63,6 +67,10 @@ namespace SteamKit2
 
             handlers.Remove( handler );
         }
+        /// <summary>
+        /// Removes a registered handler.
+        /// </summary>
+        /// <param name="handler">The handler to remove.</param>
         public void RemoveHandler( ClientMsgHandler handler )
         {
             this.RemoveHandler( handler.Name );
@@ -71,7 +79,7 @@ namespace SteamKit2
         /// <summary>
         /// Returns a registered handler by name.
         /// </summary>
-        /// <typeparam name="T">The type of the handler to cast to. Must derive from ClientMsgHandler</typeparam>
+        /// <typeparam name="T">The type of the handler to cast to. Must derive from ClientMsgHandler.</typeparam>
         /// <param name="name">The name of the handler to get.</param>
         /// <returns>A registered handler on success, or null if the handler could not be found.</returns>
         public T GetHandler<T>( string name ) where T : ClientMsgHandler
@@ -81,24 +89,98 @@ namespace SteamKit2
 
             return null;
         }
+        #endregion
 
 
+        #region Callbacks
         /// <summary>
-        /// Gets the next callback object in the queue. This function does not dequeue the callback, you must call FreeLastCallback after processing it.
+        /// Gets the next callback object in the queue.
+        /// This function does not dequeue the callback, you must call FreeLastCallback after processing it.
         /// </summary>
         /// <returns>The next callback in the queue, or null if no callback is waiting.</returns>
         public CallbackMsg GetCallback()
         {
-            CallbackMsg msg = null;
-
+            return GetCallback( false );
+        }
+        /// <summary>
+        /// Gets the next callback object in the queue, and optionally frees it.
+        /// </summary>
+        /// <param name="freeLast">if set to <c>true</c> this function also frees the last callback if one existed.</param>
+        /// <returns>The next callback in the queue, or null if no callback is waiting.</returns>
+        public CallbackMsg GetCallback( bool freeLast )
+        {
             lock ( callbackLock )
             {
                 if ( callbackQueue.Count > 0 )
-                    msg = callbackQueue.Peek();
+                    return ( freeLast ? callbackQueue.Dequeue() : callbackQueue.Peek() );
             }
 
-            return msg;
+            return null;
         }
+
+        /// <summary>
+        /// Blocks the calling thread until a callback object is posted to the queue.
+        /// This function does not dequeue the callback, you must call FreeLastCallback after processing it.
+        /// </summary>
+        /// <returns>The callback object from the queue.</returns>
+        public CallbackMsg WaitForCallback()
+        {
+            return WaitForCallback( false );
+        }
+        /// <summary>
+        /// Blocks the calling thread until a callback object is posted to the queue, or null after the timeout has elapsed.
+        /// This function does not dequeue the callback, you must call FreeLastCallback after processing it.
+        /// </summary>
+        /// <param name="timeout">The length of time to block.</param>
+        /// <returns>A callback object from the queue if a callback has been posted, or null if the timeout has elapsed.</returns>
+        public CallbackMsg WaitForCallback( TimeSpan timeout )
+        {
+            lock ( callbackLock )
+            {
+                if ( callbackQueue.Count == 0 )
+                {
+                    if ( !Monitor.Wait( callbackLock, timeout ) )
+                        return null;
+                }
+
+                return callbackQueue.Peek();
+            }
+        }
+        /// <summary>
+        /// Blocks the calling thread until a callback object is posted to the queue, and optionally frees it.
+        /// </summary>
+        /// <param name="freeLast">if set to <c>true</c> this function also frees the last callback.</param>
+        /// <returns>The callback object from the queue.</returns>
+        public CallbackMsg WaitForCallback( bool freeLast )
+        {
+            lock ( callbackLock )
+            {
+                if ( callbackQueue.Count == 0 )
+                    Monitor.Wait( callbackLock );
+
+                return ( freeLast ? callbackQueue.Dequeue() : callbackQueue.Peek() );
+            }
+        }
+        /// <summary>
+        /// Blocks the calling thread until a callback object is posted to the queue, and optionally frees it.
+        /// </summary>
+        /// <param name="freeLast">if set to <c>true</c> this function also frees the last callback.</param>
+        /// <param name="timeout">The length of time to block.</param>
+        /// <returns>A callback object from the queue if a callback has been posted, or null if the timeout has elapsed.</returns>
+        public CallbackMsg WaitForCallback( bool freeLast, TimeSpan timeout )
+        {
+            lock ( callbackLock )
+            {
+                if ( callbackQueue.Count == 0 )
+                {
+                    if ( !Monitor.Wait( callbackLock, timeout ) )
+                        return null;
+                }
+
+                return ( freeLast ? callbackQueue.Dequeue() : callbackQueue.Peek() );
+            }
+        }
+
         /// <summary>
         /// Frees the last callback in the queue.
         /// </summary>
@@ -112,10 +194,11 @@ namespace SteamKit2
                 callbackQueue.Dequeue();
             }
         }
+
         /// <summary>
-        /// Posts a callback to the queue. This is normally used by client message handlers.
+        /// Posts a callback to the queue. This is normally used directly by client message handlers.
         /// </summary>
-        /// <param name="msg">The MSG.</param>
+        /// <param name="msg">The message.</param>
         public void PostCallback( CallbackMsg msg )
         {
             if ( msg == null )
@@ -124,9 +207,10 @@ namespace SteamKit2
             lock ( callbackLock )
             {
                 callbackQueue.Enqueue( msg );
+                Monitor.Pulse( callbackLock );
             }
         }
-
+        #endregion
 
 
         protected override void OnClientMsgReceived( ClientMsgEventArgs e )
