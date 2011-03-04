b629b833e1a388eb657922a4bb93d24da7dddefa
Ryan Stecker <voidedweasel@gmail.com>
2011-03-04 04:39:37 +0000

    Refactored and started writing some xml documentation for the blob code.
    --HG--
    extra : convert_revision : svn%3A946a0da7-ebce-4904-9acb-2f1e67aed693%40183

--- SteamKit2/SteamKit2/Blob/Blob.cs
+++ SteamKit2/SteamKit2/Blob/Blob.cs
@@ -14,29 +14,65 @@ using System.Security.Cryptography;
 
 namespace SteamKit2
 {
+    /// <summary>
+    /// Represents an immutable blob object.
+    /// </summary>
     public class Blob
     {
+        /// <summary>
+        /// Gets or the fields the blob contains.
+        /// </summary>
+        /// <value>The fields.</value>
         public List<BlobField> Fields { get; private set; }
 
+        /// <summary>
+        /// Gets the <see cref="SteamKit2.BlobField"/> with the specified descriptor.
+        /// </summary>
+        /// <value>The integer descriptor to lookup.</value>
         public BlobField this[ int descriptor ]
         {
             get { return LookupField( descriptor ); }
         }
-
+        /// <summary>
+        /// Gets the <see cref="SteamKit2.BlobField"/> with the specified descriptor.
+        /// </summary>
+        /// <value>The string descriptor to lookup.</value>
         public BlobField this[ string descriptor ]
         {
             get { return LookupField( descriptor ); }
         }
 
-        public ECacheState CacheState { get; set; }
-        public EAutoPreprocessCode ProcessCode { get; set; }
+        /// <summary>
+        /// Gets the cache state of the blob.
+        /// </summary>
+        /// <value>The cache state.</value>
+        public ECacheState CacheState { get; private set; }
+        /// <summary>
+        /// Gets the process code of the blob.
+        /// </summary>
+        /// <value>The process code.</value>
+        public EAutoPreprocessCode ProcessCode { get; private set; }
 
-        public int CompressionLevel { get; set; }
-        public byte[] IV { get; set; }
+        /// <summary>
+        /// Gets the compression level of the blob.
+        /// </summary>
+        /// <value>The compression level.</value>
+        public int CompressionLevel { get; internal set; }
+        /// <summary>
+        /// Gets the IV used when encrypting or decrypting the blob.
+        /// </summary>
+        /// <value>The IV.</value>
+        public byte[] IV { get; internal set; }
 
-        public byte[] Spare { get; set; }
+        /// <summary>
+        /// Gets the spare data from parsing the blob.
+        /// </summary>
+        /// <value>The spare data.</value>
+        public byte[] Spare { get; internal set; }
 
-        public Blob(ECacheState state, EAutoPreprocessCode code)
+
+        // disallowing manual blob creation until the blob code is roundtrip
+        internal Blob( ECacheState state, EAutoPreprocessCode code )
         {
             Fields = new List<BlobField>();
 
@@ -44,12 +80,124 @@ namespace SteamKit2
             ProcessCode = code;
         }
 
-        public void AddField( BlobField field )
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Blob"/> class.
+        /// </summary>
+        /// <param name="data">The blob data.</param>
+        public Blob( byte[] data )
+        {
+            Blob blob = BlobParser.ParseBlob( data );
+
+            this.ProcessCode = blob.ProcessCode;
+            this.CacheState = blob.CacheState;
+
+            this.Fields = blob.Fields;
+
+            this.CompressionLevel = blob.CompressionLevel;
+            this.IV = blob.IV;
+
+            this.Spare = blob.Spare;
+        }
+
+
+        /// <summary>
+        /// Gets the raw data for a field with a specific string descriptor.
+        /// </summary>
+        /// <param name="descriptor">The descriptor.</param>
+        /// <returns>Data if the specific field contains any data; otherwise null.</returns>
+        public byte[] GetDescriptor( string descriptor )
+        {
+            BlobField field = LookupField( descriptor );
+
+            if ( field == null || field.HasChildBlob() )
+                return null;
+
+            return field.Descriptor;
+        }
+        /// <summary>
+        /// Gets the raw data for a field with a specific integer descriptor.
+        /// </summary>
+        /// <param name="descriptor">The descriptor.</param>
+        /// <returns>Data if the specific field contains any data; otherwise null.</returns>
+        public byte[] GetDescriptor( int descriptor )
+        {
+            BlobField field = LookupField( descriptor );
+
+            if ( field == null || field.HasChildBlob() )
+                return null;
+
+            return field.Data;
+        }
+
+
+        /// <summary>
+        /// Gets the string data for a field with a specific string descriptor.
+        /// </summary>
+        /// <param name="descriptor">The descriptor.</param>
+        /// <returns>String data if the specific field contains string data; otherwise null.</returns>
+        public string GetStringDescriptor( string descriptor )
+        {
+            BlobField field = LookupField( descriptor );
+
+            if ( field == null || field.HasChildBlob() )
+                return null;
+
+            return field.GetStringData();
+        }
+
+        /// <summary>
+        /// Gets the string data for a field with a specific integer descriptor.
+        /// </summary>
+        /// <param name="descriptor">The descriptor.</param>
+        /// <returns>String data if the specific field contains string data; otherwise null.</returns>
+        public string GetStringDescriptor( int descriptor )
+        {
+            BlobField field = LookupField( descriptor );
+
+            if ( field == null || field.HasChildBlob() || !field.IsStringData() )
+                return null;
+
+            return field.GetStringData();
+        }
+
+
+        /// <summary>
+        /// Gets the child blob for a field with a specific string descriptor.
+        /// </summary>
+        /// <param name="descriptor">The descriptor.</param>
+        /// <returns>A blob oject if the specific field contains a childblob; otherwise, null.</returns>
+        public Blob GetBlobDescriptor( string descriptor )
+        {
+            BlobField field = LookupField( descriptor );
+
+            if ( field == null || !field.HasChildBlob() )
+                return null;
+
+            return field.GetChildBlob();
+        }
+
+        /// <summary>
+        /// Gets the child blob for a field with a specific integer descriptor.
+        /// </summary>
+        /// <param name="descriptor">The descriptor.</param>
+        /// <returns>A blob oject if the specific field contains a childblob; otherwise, null.</returns>
+        public Blob GetBlobDescriptor( int descriptor )
+        {
+            BlobField field = LookupField( descriptor );
+
+            if ( field == null || !field.HasChildBlob() )
+                return null;
+
+            return field.GetChildBlob();
+        }
+
+
+        internal void AddField( BlobField field )
         {
             Fields.Add( field );
         }
 
-        private BlobField LookupField(string descriptor)
+        BlobField LookupField( string descriptor )
         {
             foreach ( BlobField field in Fields )
             {
@@ -62,8 +210,7 @@ namespace SteamKit2
 
             return null;
         }
-
-        private BlobField LookupField(int descriptor)
+        BlobField LookupField( int descriptor )
         {
             foreach ( BlobField field in Fields )
             {
@@ -74,69 +221,6 @@ namespace SteamKit2
             }
 
             return null;
-        }
-
-
-        public byte[] GetDescriptor( string descriptor )
-        {
-            BlobField field = LookupField( descriptor );
-
-            if ( field == null || field.HasBlobChild() )
-                return null;
-
-            return field.Descriptor;
-        }
-
-        public byte[] GetDescriptor( int descriptor )
-        {
-            BlobField field = LookupField( descriptor );
-
-            if ( field == null || field.HasBlobChild() )
-                return null;
-
-            return field.Data;
-        }
-
-
-        public string GetStringDescriptor( string descriptor )
-        {
-            BlobField field = LookupField( descriptor );
-
-            if ( field == null || field.HasBlobChild() )
-                return null;
-
-            return field.GetStringData();
-        }
-
-        public string GetStringDescriptor( int descriptor )
-        {
-            BlobField field = LookupField( descriptor );
-
-            if ( field == null || field.HasBlobChild() )
-                return null;
-
-            return field.GetStringData();
-        }
-
-
-        public Blob GetBlobDescriptor( string descriptor )
-        {
-            BlobField field = LookupField( descriptor );
-
-            if ( field == null || !field.HasBlobChild() )
-                return null;
-
-            return field.GetChildBlob();
-        }
-
-        public Blob GetBlobDescriptor( int descriptor )
-        {
-            BlobField field = LookupField( descriptor );
-
-            if ( field == null || !field.HasBlobChild() )
-                return null;
-
-            return field.GetChildBlob();
         }
     }
 
