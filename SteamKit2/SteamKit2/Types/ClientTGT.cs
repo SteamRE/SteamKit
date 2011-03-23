bcfcb6ebea7defa6d7120e5487ab32da02e130f5
Ryan Stecker <voidedweasel@gmail.com>
2011-03-23 04:26:44 +0000

    Added MsgClientFromGC and MsgClientToGC. Fixed a small issue in the OpenStorage code in ContentServerClient. Added documentation to ClientTGT.
    --HG--
    extra : convert_revision : svn%3A946a0da7-ebce-4904-9acb-2f1e67aed693%40217

--- SteamKit2/SteamKit2/Types/ClientTGT.cs
+++ SteamKit2/SteamKit2/Types/ClientTGT.cs
@@ -9,38 +9,67 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Runtime.InteropServices;
+using System.IO;
 
 namespace SteamKit2
 {
+
+    /// <summary>
+    /// Represents a client readable Ticket Granting Ticket (TGT) from the Steam2 network
+    /// </summary>
     public sealed class ClientTGT
     {
-        public byte[] AccountRecordKey;
+        /// <summary>
+        /// Gets the 128 bit AES key used when decrypting the account record blob.
+        /// </summary>
+        /// <value>128 bit AES key.</value>
+        public byte[] AccountRecordKey { get; set; }
 
-        public SteamGlobalUserID UserID;
+        /// <summary>
+        /// Gets the Steam2 UserID.
+        /// </summary>
+        /// <value>The UserID.</value>
+        public SteamGlobalUserID UserID { get; set; }
 
-        public IPAddrPort Server1;
-        public IPAddrPort Server2;
+        public IPAddrPort Server1 { get; set; }
+        public IPAddrPort Server2 { get; set; }
 
-        public MicroTime CreationTime;
-        public MicroTime ExpirationTime;
+        /// <summary>
+        /// Gets the creation time of this TGT.
+        /// </summary>
+        /// <value>The creation time.</value>
+        public MicroTime CreationTime { get; set; }
+        /// <summary>
+        /// Gets the expiration time of this TGT.
+        /// </summary>
+        /// <value>The expiration time.</value>
+        public MicroTime ExpirationTime { get; set; }
 
 
+        /// <summary>
+        /// Deserializes a ClientTGT from a block of data.
+        /// </summary>
+        /// <param name="data">The data.</param>
+        /// <returns>A ClientTGT.</returns>
         public static ClientTGT Deserialize( byte[] data )
         {
-            DataStream ds = new DataStream( data );
-            ClientTGT tgt = new ClientTGT();
+            using ( MemoryStream ms = new MemoryStream( data ) )
+            using ( BinaryReader br = new BinaryReader( ms ) )
+            {
+                ClientTGT tgt = new ClientTGT();
 
-            tgt.AccountRecordKey = ds.ReadBytes( 16 );
+                tgt.AccountRecordKey = br.ReadBytes( 16 );
 
-            tgt.UserID = SteamGlobalUserID.Deserialize( ds.ReadBytes( 10 ) );
+                tgt.UserID = SteamGlobalUserID.Deserialize( br.ReadBytes( 10 ) );
 
-            tgt.Server1 = IPAddrPort.Deserialize( ds.ReadBytes( 6 ) );
-            tgt.Server2 = IPAddrPort.Deserialize( ds.ReadBytes( 6 ) );
+                tgt.Server1 = IPAddrPort.Deserialize( br.ReadBytes( 6 ) );
+                tgt.Server2 = IPAddrPort.Deserialize( br.ReadBytes( 6 ) );
 
-            tgt.CreationTime = MicroTime.Deserialize( ds.ReadBytes( 8 ) );
-            tgt.ExpirationTime = MicroTime.Deserialize( ds.ReadBytes( 8 ) );
+                tgt.CreationTime = MicroTime.Deserialize( br.ReadBytes( 8 ) );
+                tgt.ExpirationTime = MicroTime.Deserialize( br.ReadBytes( 8 ) );
 
-            return tgt;
+                return tgt;
+            }
         }
     }
 }
