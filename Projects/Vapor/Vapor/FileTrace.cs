1bf30e88243ca5ca4c2f721f6a7028d49a2ea1f9
Ryan Stecker <voidedweasel@gmail.com>
2011-05-07 23:19:41 +0000

    -debug now logs to a "debug.log" file.
    --HG--
    extra : convert_revision : svn%3A946a0da7-ebce-4904-9acb-2f1e67aed693%40240

---
+++ Projects/Vapor/Vapor/FileTrace.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SteamKit2;
+using System.IO;
+
+namespace Vapor
+{
+    class FileTrace : IDebugListener
+    {
+        const string LogFile = "debug.log";
+
+        public FileTrace()
+        {
+            DebugLog.AddListener( this );
+
+            try
+            {
+                File.AppendAllText( LogFile, Environment.NewLine + Environment.NewLine );
+                File.AppendAllText( LogFile, string.Format( "New log started on {0} at {1}" + Environment.NewLine, DateTime.Now.ToLongDateString(), DateTime.Now.ToLongTimeString() ) );
+            }
+            catch { }
+        }
+
+        public void WriteLine( string msg )
+        {
+            try
+            {
+                File.AppendAllText( LogFile, string.Format( "{0}" + Environment.NewLine, msg ) );
+            }
+            catch { }
+        }
+    }
+}
