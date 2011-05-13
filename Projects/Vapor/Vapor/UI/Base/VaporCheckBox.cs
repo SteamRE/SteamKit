83d5172c5cd905cf196d91efc9c97386422216c6
Ryan Stecker <voidedweasel@gmail.com>
2011-05-13 19:52:19 +0000

    Vapor can now logon to the alternate account instance, keeping the original logon alive.
    --HG--
    extra : convert_revision : svn%3A946a0da7-ebce-4904-9acb-2f1e67aed693%40243

---
+++ Projects/Vapor/Vapor/UI/Base/VaporCheckBox.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Vapor
+{
+    class VaporCheckBox : CheckBox
+    {
+        public VaporCheckBox()
+        {
+            this.ForeColor = Color.White;
+        }
+    }
+}
