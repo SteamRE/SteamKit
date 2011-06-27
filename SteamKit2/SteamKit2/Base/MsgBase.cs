76d01cc73a6b4b0fde3dc89c601d9e8a90dcb8ea
Ryan Stecker <voidedweasel@gmail.com>
2011-06-27 00:03:42 -0500

    Added GCMsg<MsgType, Hdr> and GCMsgProtobuf<MsgType>. Latest generated SteamLanguage. Updated proj/sln with latest GC changes.

--- SteamKit2/SteamKit2/Base/MsgBase.cs
+++ SteamKit2/SteamKit2/Base/MsgBase.cs
@@ -17,6 +17,60 @@ namespace SteamKit2
         void SetData( byte[] data );
     }
 
+    public class GCMsg<MsgType, Hdr> : IClientMsg
+        where Hdr : IGCSerializableHeader, new()
+        where MsgType : IGCSerializableMessage, new()
+    {
+                public Hdr Header { get; private set; }
+        public MsgType Msg { get; private set; }
+
+        public BinaryWriterEx Payload { get; private set; }
+
+        public GCMsg()
+        {
+            Header = new Hdr();
+            Msg = new MsgType();
+            Payload = new BinaryWriterEx();
+
+            Header.SetEMsg( Msg.GetEMsg() );
+        }
+
+        public GCMsg( byte[] data )
+            : this()
+        {
+            this.SetData( data );
+        }
+
+        public void SetData( byte[] data )
+        {
+            MemoryStream ms = new MemoryStream( data );
+
+            Header.Deserialize( ms );
+            Msg.Deserialize( ms );
+
+            byte[] payload = new byte[ ms.Length - ms.Position ];
+            ms.Read( payload, 0, payload.Length );
+
+            Payload.Write( payload );
+        }
+
+
+        public EGCMsg GetEMsg()
+        {
+            return Msg.GetEMsg();
+        }
+
+        public void Serialize(Stream s)
+        {
+            BinaryWriterEx bb = new BinaryWriterEx(s);
+
+            Header.Serialize(bb);
+            Msg.Serialize(bb);
+
+            bb.Write(Payload.ToArray());
+        }
+    }
+
     public class ClientMsg<MsgType, Hdr> : IClientMsg
         where Hdr : ISteamSerializableHeader, new()
         where MsgType : ISteamSerializableMessage, new()
@@ -75,7 +129,7 @@ namespace SteamKit2
     public class ClientMsgProtobuf<MsgType> : ClientMsg<MsgType, MsgHdrProtoBuf>
         where MsgType : ISteamSerializableMessage, new()
     {
-        public CMsgProtoBufHeader ProtoHeader
+        public SteamKit2.CMsgProtoBufHeader ProtoHeader
         {
             get
             {
@@ -89,6 +143,25 @@ namespace SteamKit2
         }
 
         public ClientMsgProtobuf( byte[] data )
+            : base( data )
+        {
+        }
+    }
+
+    public class GCMsgProtobuf<MsgType> : GCMsg<MsgType, MsgGCHdrProtoBuf>
+        where MsgType : IGCSerializableMessage, new()
+    {
+        public SteamKit2.GC.CMsgProtoBufHeader ProtoHeader
+        {
+            get { return Header.ProtoHeader; }
+        }
+
+        public GCMsgProtobuf()
+            : base()
+        {
+        }
+
+        public GCMsgProtobuf( byte[] data )
             : base( data )
         {
         }
