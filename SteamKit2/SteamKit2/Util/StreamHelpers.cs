9237266125ed4e4ec1803a691351b9a4d656ad54
Ryan Stecker <voidedweasel@gmail.com>
2011-07-02 22:18:44 -0500

    Moved StreamHelpers into Util. Added a KeyValue parser. (thanks Rick)
    --HG--
    rename : SteamKit2/SteamKit2/Blob/StreamHelpers.cs => SteamKit2/SteamKit2/Util/StreamHelpers.cs

--- SteamKit2/SteamKit2/Util/StreamHelpers.cs
+++ SteamKit2/SteamKit2/Util/StreamHelpers.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Text;
 
 namespace SteamKit2
 {
@@ -22,6 +23,35 @@ namespace SteamKit2
                stream.ReadByte() << 16 | stream.ReadByte() << 24 |
                stream.ReadByte() << 32 | stream.ReadByte() << 40 |
                stream.ReadByte() << 48 | stream.ReadByte() << 56);
+        }
+
+        internal static float ReadFloat( this Stream stream )
+        {
+            return ( float )stream.ReadInt32();
+        }
+
+        internal static string ReadNullTermString( this Stream stream, Encoding encoding )
+        {
+            int characterSize = encoding.GetByteCount( "e" );
+
+            using ( MemoryStream ms = new MemoryStream() )
+            {
+
+                while ( true )
+                {
+                    byte[] data = new byte[ characterSize ];
+                    stream.Read( data, 0, characterSize );
+
+                    if ( encoding.GetString( data, 0, characterSize ) == "\0" )
+                    {
+                        break;
+                    }
+
+                    ms.Write( data, 0, data.Length );
+                }
+
+                return encoding.GetString( ms.ToArray() );
+            }
         }
 
         internal static byte[] ReadBytes( this Stream stream, int len )
