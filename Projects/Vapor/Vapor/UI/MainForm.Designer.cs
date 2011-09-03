322fd9a0b6f496d919ad537e804f618f8b84480d
Ryan Stecker <voidedweasel@gmail.com>
2011-09-03 17:25:17 -0500

    Doubling clicking the tray icon now toggles visibility on the main window.

--- Projects/Vapor/Vapor/UI/MainForm.Designer.cs
+++ Projects/Vapor/Vapor/UI/MainForm.Designer.cs
@@ -169,6 +169,7 @@
             this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
             this.notifyIcon1.Text = "Vapor";
             this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
             // 
             // vaporContextMenu2
             // 
