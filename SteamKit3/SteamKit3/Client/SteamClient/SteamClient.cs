05df34cd80e3f6b23ec9b6f85d85d3ffc8fa861f
Ryan Stecker <voidedweasel@gmail.com>
2011-09-14 04:47:56 -0500

    Refactored incoming packet message handling so that CMClient is responsible for building packets.

--- SteamKit3/SteamKit3/Client/SteamClient/SteamClient.cs
+++ SteamKit3/SteamKit3/Client/SteamClient/SteamClient.cs
@@ -10,9 +10,14 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Reflection;
+using log4net;
 
 namespace SteamKit3
 {
+    /// <summary>
+    /// Represents a single client that connects to the Steam3 network.
+    /// This class is also responsible for handling the registration of client message handlers and callbacks.
+    /// </summary>
     public sealed partial class SteamClient : CMClient
     {
         static List<Type> registeredHandlers = new List<Type>();
@@ -20,11 +25,21 @@ namespace SteamKit3
         Dictionary<Type, ClientHandler> handlers;
         Queue<CallbackMsg> callbackQueue;
 
+        static readonly ILog log = LogManager.GetLogger( typeof( SteamClient ) );
 
+
+        /// <summary>
+        /// Gets the <see cref="JobMgr"/> for this <see cref="SteamClient"/> instance.
+        /// </summary>
         public JobMgr JobMgr { get; private set; }
 
 
-        public SteamClient()
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SteamClient"/> class using a specific connection type.
+        /// </summary>
+        /// <param name="connType">The connection type to use.</param>
+        public SteamClient( ConnectionType connType = ConnectionType.Tcp )
+            : base( connType )
         {
             handlers = new Dictionary<Type, ClientHandler>();
             callbackQueue = new Queue<CallbackMsg>();
@@ -42,52 +57,43 @@ namespace SteamKit3
         }
 
 
+        /// <summary>
+        /// Called when the client successfully connects and the channel is encrypted.
+        /// </summary>
         protected override void OnClientConnected()
         {
             // todo: when connected on the non-encrypted port (27014?) this should post a ConnectedCallback, since the remote server
             // will never attempt a channel handshake, and will wait on the client to do something
         }
+        /// <summary>
+        /// Called when the client is disconnected.
+        /// </summary>
         protected override void OnClientDisconnected()
         {
             this.PostCallback( new DisconnectedCallback() );
         }
-        protected override void OnClientMsgReceived( NetMsgEventArgs e )
+        /// <summary>
+        /// Called when the client receieves a network message.
+        /// </summary>
+        /// <param name="msg">The packet message.</param>
+        protected override void OnClientMsgReceived( IPacketMsg msg )
         {
-            var packetMsg = GetPacketMsg( e.Data );
-
-            JobMgr.RouteMsgToJob( packetMsg );
+            JobMgr.RouteMsgToJob( msg );
         }
 
+
+        // internal for access from the MultiplexMultiJob
         internal void OnMulti( byte[] data )
         {
-            OnClientMsgReceived( new NetMsgEventArgs( data, null ) );
-        }
-
-        IPacketMsg GetPacketMsg( byte[] data )
-        {
-            uint rawEMsg = BitConverter.ToUInt32( data, 0 );
-            EMsg eMsg = MsgUtil.GetMsg( rawEMsg );
-
-            switch ( eMsg )
-            {
-                case EMsg.ChannelEncryptRequest:
-                case EMsg.ChannelEncryptResponse:
-                case EMsg.ChannelEncryptResult:
-                    return new PacketMsg( eMsg, data );
-            }
-
-            if ( MsgUtil.IsProtoBuf( rawEMsg ) )
-            {
-                return new PacketClientMsgProtobuf( eMsg, data );
-            }
-            else
-            {
-                return new PacketClientMsg( eMsg, data );
-            }
+            OnClientMsgReceived( GetPacketMsg( data ) );
         }
 
 
         #region Callbacks
+        /// <summary>
+        /// Posts a callback to the queue. This is normally used directly by client jobs.
+        /// </summary>
+        /// <param name="msg">The message.</param>
         public void PostCallback( CallbackMsg msg )
         {
             lock ( callbackQueue )
@@ -97,6 +103,10 @@ namespace SteamKit3
             }
         }
 
+        /// <summary>
+        /// Gets the next callback object in the queue.
+        /// </summary>
+        /// <returns>The next callback in the queue, or null if no callback is waiting.</returns>
         public CallbackMsg GetCallback()
         {
             lock ( callbackQueue )
@@ -107,23 +117,20 @@ namespace SteamKit3
                 return null;
             }
         }
-        public CallbackMsg WaitForCallback()
-        {
-            lock ( callbackQueue )
-            {
-                while ( callbackQueue.Count == 0 )
-                    Monitor.Wait( callbackQueue );
-
-                return callbackQueue.Dequeue();
-            }
-        }
-        public CallbackMsg WaitForCallback( TimeSpan timeout )
+        /// <summary>
+        /// Blocks the calling thread until a callback object is posted to the queue, or null after the timeout has elapsed.
+        /// </summary>
+        /// <param name="timeout">The length of time to block, or null to block until a callback is posted.</param>
+        /// <returns>A callback object from the queue if a callback has been posted, or null if the timeout has elapsed.</returns>
+        public CallbackMsg WaitForCallback( TimeSpan? timeout = null )
         {
             lock ( callbackQueue )
             {
                 while ( callbackQueue.Count == 0 )
                 {
-                    if ( !Monitor.Wait( callbackQueue, timeout ) )
+                    TimeSpan waitTime = timeout.GetValueOrDefault( TimeSpan.FromMilliseconds( -1 ) );
+
+                    if ( !Monitor.Wait( callbackQueue, waitTime ) )
                         return null;
                 }
 
@@ -133,6 +140,10 @@ namespace SteamKit3
         #endregion
 
         #region Handlers
+        /// <summary>
+        /// Registers all available handlers in the given assembly.
+        /// </summary>
+        /// <param name="assembly">The assembly.</param>
         public static void RegisterHandlers( Assembly assembly )
         {
             foreach ( var type in assembly.GetTypes() )
@@ -146,6 +157,11 @@ namespace SteamKit3
             }
         }
 
+        /// <summary>
+        /// Returns a registered handler.
+        /// </summary>
+        /// <typeparam name="T">The type of the handler to cast to. Must derive from <see cref="ClientHandler"/>.</typeparam>
+        /// <returns>A registered handler on success, or null if the handler could not be found.</returns>
         public T GetHandler<T>()
             where T : ClientHandler
         {
@@ -160,6 +176,8 @@ namespace SteamKit3
 
         void SetupHandlers()
         {
+            // create a new instance of every handler we know about for this client
+
             foreach ( var type in registeredHandlers )
             {
                 ClientHandler handler = Activator.CreateInstance( type, true ) as ClientHandler;
