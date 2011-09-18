522600256eb40d46cd46e00e1b0ce99a58d5fd05
Ryan Stecker <voidedweasel@gmail.com>
2011-09-18 05:05:05 -0500

    Added DepotDownloader2, a rewrite of DepotDownloader to be more of a hldsupdatetool replacement.

---
+++ Projects/DepotDownloader2/DepotDownloader2/Steam2/ServerCache.cs
@@ -0,0 +1,80 @@
+﻿/*
+ * This file is subject to the terms and conditions defined in
+ * file 'license.txt', which is part of this source code package.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using SteamKit2;
+
+namespace DepotDownloader2
+{
+    class ServerCache
+    {
+        public static ServerList ConfigServers { get; private set; }
+        public static ServerList CSDSServers { get; private set; }
+
+
+        static ServerCache()
+        {
+            ConfigServers = new ServerList();
+            CSDSServers = new ServerList();
+        }
+
+
+        public static void Build()
+        {
+            Log.WriteLine( "Building Steam2 server cache..." );
+
+            foreach ( var gdServer in GeneralDSClient.GDServers )
+            {
+                BuildServer( gdServer, EServerType.ConfigServer, ConfigServers );
+                BuildServer( gdServer, EServerType.CSDS, CSDSServers );
+            }
+
+            Log.WriteLine( "Done! {0} Config Servers, {1} CSDS Servers.\n", ConfigServers.Count, CSDSServers.Count );
+        }
+
+        static void BuildServer( IPEndPoint gdServer, EServerType serverType, ServerList listToBuild )
+        {
+            try
+            {
+                GeneralDSClient gdsClient = new GeneralDSClient();
+                gdsClient.Connect( gdServer );
+
+                IPEndPoint[] servers = gdsClient.GetServerList( serverType );
+
+                listToBuild.AddRange( servers );
+
+                gdsClient.Disconnect();
+            }
+            catch ( Exception ex )
+            {
+                Log.WriteVerbose( "Warning: Unable to connect to GDS {0} to get a list of {1} servers: {2}", gdServer, serverType, ex.Message );
+            }
+        }
+    }
+
+
+    class ServerList : List<IPEndPoint>
+    {
+        public new void AddRange( IEnumerable<IPEndPoint> endPoints )
+        {
+            foreach ( var endPoint in endPoints )
+            {
+                Add( endPoint );
+            }
+        }
+
+        public new void Add( IPEndPoint endPoint )
+        {
+            if ( base.Contains( endPoint ) )
+                return;
+
+            base.Add( endPoint );
+        }
+    }
+}
---
+++ Projects/DepotDownloader2/DepotDownloader2/Utils/Log.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * This file is subject to the terms and conditions defined in
+ * file 'license.txt', which is part of this source code package.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DepotDownloader2
+{
+    static class Log
+    {
+        public static void WriteLine( string format, params object[] args )
+        {
+            Console.WriteLine( format, args );
+        }
+        public static void WriteLine()
+        {
+            Console.WriteLine();
+        }
+
+        public static void WriteVerbose( string format, params object[] args )
+        {
+            if ( !Options.Verbose )
+                return;
+
+            WriteLine( format, args );
+        }
+    }
+}
