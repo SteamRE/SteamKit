933f21e9f4a27da89e4b3eda55c3fcfbb851ce78
Ryan Stecker <voidedweasel@gmail.com>
2011-09-18 05:12:45 -0500

    Added mising/incorrect assembly info.

--- Projects/DepotDownloader2/DepotDownloader2/Properties/AssemblyInfo.cs
+++ Projects/DepotDownloader2/DepotDownloader2/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle( "DepotDownloader2" )]
-[assembly: AssemblyDescription( "" )]
+[assembly: AssemblyDescription( "Valve Gameserver Download Tool" )]
 [assembly: AssemblyConfiguration( "" )]
-[assembly: AssemblyCompany( "Microsoft" )]
+[assembly: AssemblyCompany( "Ryan Stecker" )]
 [assembly: AssemblyProduct( "DepotDownloader2" )]
-[assembly: AssemblyCopyright( "Copyright © Microsoft 2011" )]
+[assembly: AssemblyCopyright( "Copyright © Ryan Stecker 2011" )]
 [assembly: AssemblyTrademark( "" )]
 [assembly: AssemblyCulture( "" )]
 
@@ -32,5 +32,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion( "1.0.0.0" )]
-[assembly: AssemblyFileVersion( "1.0.0.0" )]
+[assembly: AssemblyVersion( "0.0.1.*" )]
