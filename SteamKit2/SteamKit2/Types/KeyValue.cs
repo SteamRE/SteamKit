9a019bcac58ca59b4fb4c0632f4bdb515245eaf2
Ryan Stecker <voidedweasel@gmail.com>
2011-12-06 00:22:15 -0600

    Added missing license headers.

--- SteamKit2/SteamKit2/Types/KeyValue.cs
+++ SteamKit2/SteamKit2/Types/KeyValue.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿/*
+ * This file is subject to the terms and conditions defined in
+ * file 'license.txt', which is part of this source code package.
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
