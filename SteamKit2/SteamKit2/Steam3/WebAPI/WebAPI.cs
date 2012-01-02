370129a186fb7a2b3099355cbde2f1ed7bc24242
Ryan Stecker <voidedweasel@gmail.com>
2012-01-02 15:29:56 -0600

    Reworked WebAPI to make use dynamic features.

--- SteamKit2/SteamKit2/Steam3/WebAPI/WebAPI.cs
+++ SteamKit2/SteamKit2/Steam3/WebAPI/WebAPI.cs
@@ -6,13 +6,15 @@ using System.Net;
 using System.Collections.Specialized;
 using System.IO;
 using System.Collections;
+using System.Dynamic;
+using System.Text.RegularExpressions;
 
 namespace SteamKit2
 {
     public sealed class WebAPI
     {
 
-        public sealed class Interface : IDisposable
+        public sealed class Interface : DynamicObject, IDisposable
         {
             WebClient webClient;
 
@@ -20,6 +22,11 @@ namespace SteamKit2
             string apiKey;
 
             const string API_ROOT = "http://api.steampowered.com/";
+
+            static Regex funcNameRegex = new Regex(
+                @"(?<name>[a-zA-Z]+)(?<version>\d*)",
+                RegexOptions.Compiled | RegexOptions.IgnoreCase
+            );
 
 
             internal Interface( string iface, string apiKey )
@@ -67,6 +74,46 @@ namespace SteamKit2
             public void Dispose()
             {
                 webClient.Dispose();
+            }
+
+            public override bool TryInvokeMember( InvokeMemberBinder binder, object[] args, out object result )
+            {
+                if ( binder.CallInfo.ArgumentNames.Count != args.Length )
+                {
+                    throw new InvalidOperationException( "Argument mismatch in API call. All parameters must be passed as named arguments." );
+                }
+
+                var apiArgs = new Dictionary<string, string>();
+
+                // convert named arguments into key value pairs
+                for ( int x = 0 ; x < args.Length ; x++ )
+                {
+                    apiArgs.Add( binder.CallInfo.ArgumentNames[ x ], args[ x ].ToString() );
+                }
+
+                Match match = funcNameRegex.Match( binder.Name );
+
+                if ( !match.Success )
+                {
+                    throw new InvalidOperationException(
+                        "The called API function was invalid. It must be in the form of 'FunctionName###' where the optional ### represent the function version."
+                    );
+                }
+
+                string functionName = match.Groups[ "name" ].Value;
+
+                int version = 1; // assume version 1 unless specified
+                string versionString = match.Groups[ "version" ].Value;
+
+                if ( !string.IsNullOrEmpty( versionString ) )
+                {
+                    version = int.Parse( versionString );
+                }
+
+
+                result = Call( functionName, version, apiArgs );
+
+                return true;
             }
         }
 
