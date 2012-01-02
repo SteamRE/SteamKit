4bf167b4a14b5b532c1d91e7df8d484d0a886208
Ryan Stecker <voidedweasel@gmail.com>
2012-01-01 22:20:46 -0600

    Added a utility callback manager class.
    --HG--
    rename : SteamKit2/SteamKit2/Steam3/SteamClient/CallbackMsg.cs => SteamKit2/SteamKit2/Steam3/SteamClient/CallbackMgr/CallbackMsg.cs

---
+++ SteamKit2/SteamKit2/Steam3/SteamClient/CallbackMgr/CallbackMgr.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SteamKit2
+{
+    public interface ICallback
+    {
+        Type CallbackType { get; }
+        void Run( object callback );
+    }
+
+    public sealed class Callback<TCall> : ICallback
+        where TCall : CallbackMsg
+    {
+        public delegate void CallbackFunc( TCall callback );
+
+
+        CallbackFunc func;
+
+        public Type CallbackType { get { return typeof( TCall ); } }
+
+
+        public Callback( CallbackFunc func, CallbackMgr mgr )
+        {
+            this.func = func;
+            mgr.Register( this );
+        }
+
+
+        public void Run( object callback )
+        {
+            func( callback as TCall );
+        }
+    }
+
+    public sealed class CallbackMgr
+    {
+#if !STATIC_CALLBACKS
+        SteamClient client;
+#endif
+
+        List<ICallback> registeredCallbacks;
+
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CallbackMgr"/> class.
+        /// </summary>
+        /// <param name="client">The client.</param>
+#if STATIC_CALLBACKS
+        public CallbackMgr()
+#else
+        public CallbackMgr( SteamClient client )
+#endif
+        {
+            registeredCallbacks = new List<ICallback>();
+
+#if !STATIC_CALLBACKS
+            this.client = client;
+#endif
+        }
+
+
+        public void RunCallbacks()
+        {
+#if STATIC_CALLBACKS
+            var call = SteamClient.GetCallback( true );
+#else
+            var call = client.GetCallback( true );
+#endif
+
+            if ( call == null )
+                return;
+
+            Handle( call );
+        }
+        public void RunWaitCallbacks()
+        {
+#if STATIC_CALLBACKS
+            var call = SteamClient.WaitForCallback( true );
+#else
+            var call = client.WaitForCallback( true );
+#endif
+
+            Handle( call );
+        }
+
+
+        internal void Register( ICallback call )
+        {
+            registeredCallbacks.Add( call );
+        }
+
+        void Handle( CallbackMsg call )
+        {
+            registeredCallbacks
+                .FindAll( callback => callback.CallbackType == call.GetType() ) // find handlers interested in this callback
+                .ForEach( callback => callback.Run( call ) ); // run them
+        }
+    }
+}
