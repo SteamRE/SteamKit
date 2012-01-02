097fa48f812028735a418a2cba18e8e5b26a71bf
Ryan Stecker <voidedweasel@gmail.com>
2012-01-02 13:48:49 -0600

    SteamUserStats.GetNumberOfCurrentPlayers reworked to make sure of JobCallbacks.

--- SteamKit2/SteamKit2/Steam3/Handlers/SteamUserStats/SteamUserStats.cs
+++ SteamKit2/SteamKit2/Steam3/Handlers/SteamUserStats/SteamUserStats.cs
@@ -12,13 +12,18 @@ namespace SteamKit2
     public sealed partial class SteamUserStats : ClientMsgHandler
     {
 
-        public void GetNumberOfCurrentPlayers( GameID gameId )
+        public long GetNumberOfCurrentPlayers( GameID gameId )
         {
+            long jobId = Client.GetNextJobID();
+
             var msg = new ClientMsg<MsgClientGetNumberOfCurrentPlayers, ExtendedClientMsgHdr>();
+            msg.Header.SourceJobID = jobId;
 
             msg.Msg.GameID = gameId;
 
             Client.Send( msg );
+
+            return jobId;
         }
 
 
@@ -41,12 +46,13 @@ namespace SteamKit2
         void HandleNumberOfPlayersResponse( ClientMsgEventArgs e )
         {
             var msg = new ClientMsg<MsgClientGetNumberOfCurrentPlayersResponse, ExtendedClientMsgHdr>( e.Data );
-
 #if STATIC_CALLBACKS
-            var callback = new NumberOfPlayersCallback( Client, msg.Msg );
+            var innerCallback = new NumberOfPlayersCallback( Client, msg.Msg );
+            var callback = new SteamClient.JobCallback<NumberOfPlayersCallback>( Client, msg.Header.TargetJobID, innerCallback );
             SteamClient.PostCallback( callback );
 #else
-            var callback = new NumberOfPlayersCallback( msg.Msg );
+            var innerCallback = new NumberOfPlayersCallback( msg.Msg );
+            var callback = new SteamClient.JobCallback<NumberOfPlayersCallback>( msg.Header.TargetJobID, innerCallback );
             Client.PostCallback( callback );
 #endif
         }
