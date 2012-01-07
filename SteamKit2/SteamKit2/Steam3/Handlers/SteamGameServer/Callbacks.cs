fc909bcd1d90864c95b165b635531f64f6be2484
Ryan Stecker <voidedweasel@gmail.com>
2012-01-06 21:51:52 -0600

    Added documentation.

--- SteamKit2/SteamKit2/Steam3/Handlers/SteamGameServer/Callbacks.cs
+++ SteamKit2/SteamKit2/Steam3/Handlers/SteamGameServer/Callbacks.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿/*
+ * This file is subject to the terms and conditions defined in
+ * file 'license.txt', which is part of this source code package.
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,8 +12,17 @@ namespace SteamKit2
 {
     partial class SteamGameServer
     {
+        /// <summary>
+        /// This callback is fired when the game server receives a status reply.
+        /// </summary>
         public sealed class StatusReplyCallback : CallbackMsg
         {
+            /// <summary>
+            /// Gets a value indicating whether this game server is VAC secure.
+            /// </summary>
+            /// <value>
+            /// 	<c>true</c> if this server is VAC secure; otherwise, <c>false</c>.
+            /// </value>
             public bool IsSecure { get; private set; }
 
 
@@ -23,16 +37,37 @@ namespace SteamKit2
             }
         }
 
+        /// <summary>
+        /// This callback is fired when ticket authentication has completed.
+        /// </summary>
         public sealed class TicketAuthCallback : CallbackMsg
         {
+            /// <summary>
+            /// Gets the SteamID the ticket auth completed for.
+            /// </summary>
             public SteamID SteamID { get; private set; }
+            /// <summary>
+            /// Gets the GameID the ticket was for.
+            /// </summary>
             public GameID GameID { get; private set; }
 
+            /// <summary>
+            /// Gets the authentication state.
+            /// </summary>
             public uint State { get; private set; }
 
+            /// <summary>
+            /// Gets the auth session response.
+            /// </summary>
             public EAuthSessionResponse AuthSessionResponse { get; private set; }
 
+            /// <summary>
+            /// Gets the ticket CRC.
+            /// </summary>
             public uint TicketCRC { get; private set; }
+            /// <summary>
+            /// Gets the ticket sequence.
+            /// </summary>
             public uint TicketSequence { get; private set; }
 
 
