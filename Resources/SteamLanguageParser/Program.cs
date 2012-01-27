4aa70dcb5ca3c4ff5a5405ff77eb5e56bdb65d97
Ryan Stecker <voidedweasel@gmail.com>
2012-01-27 16:20:19 -0600

    Modified steamlanguageparser to generate internal and public code namespaces.

--- Resources/SteamLanguageParser/Program.cs
+++ Resources/SteamLanguageParser/Program.cs
@@ -18,11 +18,11 @@ namespace SteamLanguageParser
                 throw new Exception( "Unable to find SteamRE project path, please specify the `SteamRE` environment variable" );
             }
 
-            ParseFile( projectPath, @"Resources\SteamLanguage", "steammsg.steamd", "SteamKit2", @"SteamKit2\SteamKit2\Base\Generated\SteamLanguage.cs", true );
+            ParseFile( projectPath, @"Resources\SteamLanguage", "steammsg.steamd", "SteamKit2", @"SteamKit2\SteamKit2\Base\Generated\", "SteamLanguage", true );
 
         }
 
-        private static void ParseFile( string projectPath, string path, string file, string nspace, string outputFile, bool supportsGC )
+        private static void ParseFile( string projectPath, string path, string file, string nspace, string outputPath, string outFile, bool supportsGC )
         {
             string languagePath = Path.Combine( projectPath, path );
 
@@ -31,13 +31,29 @@ namespace SteamLanguageParser
 
             Node root = TokenAnalyzer.Analyze( tokenList );
 
-            StringBuilder sb = new StringBuilder();
-
             //JavaGen cgen = new JavaGen();
             CSharpGen cgen = new CSharpGen();
 
-            CodeGenerator.EmitCode( root, cgen, sb, nspace, supportsGC );
-            File.WriteAllText( Path.Combine( projectPath, outputFile ), sb.ToString() );
+            Node rootEnumNode = new Node();
+            Node rootMessageNode = new Node();
+
+            // this is now heavily c# based code
+            // anyone looking to modify JavaGen or crate a different generator has their work cut out for them
+
+            rootEnumNode.childNodes.AddRange( root.childNodes.Where( n => n is EnumNode ) );
+            rootMessageNode.childNodes.AddRange( root.childNodes.Where( n => n is ClassNode ) );
+
+            StringBuilder enumBuilder = new StringBuilder();
+            StringBuilder messageBuilder = new StringBuilder();
+
+            CodeGenerator.EmitCode( rootEnumNode, cgen, enumBuilder, nspace, supportsGC, false );
+            CodeGenerator.EmitCode( rootMessageNode, cgen, messageBuilder, nspace + ".Internal", supportsGC, true );
+
+            string outputEnumFile = Path.Combine( outputPath, outFile + ".cs" );
+            string outputMessageFile = Path.Combine( outputPath, outFile + "Internal.cs" );
+
+            File.WriteAllText( Path.Combine( projectPath, outputEnumFile ), enumBuilder.ToString() );
+            File.WriteAllText( Path.Combine( projectPath, outputMessageFile ), messageBuilder.ToString() );
         }
     }
 }
