8713f1507296d343a508421bac363d33b451dc20
Ryan Stecker <voidedweasel@gmail.com>
2012-05-04 22:15:47 -0500

    Updated protobuf build scripts for games.
    --HG--
    branch : gc

---
+++ Resources/Protobufs/dota/generate-base.bat
@@ -0,0 +1,7 @@
+@echo off
+
+rem the base GC messages are generated from the tf2 protobufs
+
+echo Building Dota messages...
+..\..\Protogen\protogen -s:..\ -i:"matchmaker_common.proto" -o:"..\..\..\SteamKit2\SteamKit2\Base\Generated\GC\Dota\MatchmakerCommon.cs" -t:csharp -ns:"SteamKit2.Internal.GC.Dota"
+..\..\Protogen\protogen -s:..\ -i:"dota_gcmessages.proto" -o:"..\..\..\SteamKit2\SteamKit2\Base\Generated\GC\Dota\MsgGC.cs" -t:csharp -ns:"SteamKit2.Internal.GC.Dota"
