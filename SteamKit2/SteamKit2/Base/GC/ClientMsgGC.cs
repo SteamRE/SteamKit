891f8ac4d056d68488b49e61745c186210b0fb1a
Ryan Stecker <voidedweasel@gmail.com>
2012-05-06 03:23:50 -0500

    Added ClientGCMsg to send non-protobuf gc messages.
    --HG--
    branch : gc

--- SteamKit2/SteamKit2/Base/GC/ClientMsgGC.cs
+++ SteamKit2/SteamKit2/Base/GC/ClientMsgGC.cs
@@ -152,5 +152,133 @@ namespace SteamKit2.GC
             }
         }
     }
+
+    /// <summary>
+    /// Represents a struct backed client message.
+    /// </summary>
+    /// <typeparam name="BodyType">The body type of this message.</typeparam>
+    public sealed class ClientGCMsg<BodyType> : GCMsgBase<MsgGCHdr>
+        where BodyType : IGCSerializableMessage, new()
+    {
+        /// <summary>
+        /// Gets a value indicating whether this client message is protobuf backed.
+        /// </summary>
+        /// <value>
+        /// 	<c>true</c> if this instance is protobuf backed; otherwise, <c>false</c>.
+        /// </value>
+        public override bool IsProto { get { return false; } }
+
+        uint msgType;
+        /// <summary>
+        /// Gets the network message type of this client message.
+        /// </summary>
+        /// <value>
+        /// The network message type.
+        /// </value>
+        public override uint MsgType { get { return msgType; } }
+
+        /// <summary>
+        /// Gets or sets the target job id for this client message.
+        /// </summary>
+        /// <value>
+        /// The target job id.
+        /// </value>
+        public override ulong TargetJobID
+        {
+            get { return Header.TargetJobID; }
+            set { Header.TargetJobID = value; }
+        }
+        /// <summary>
+        /// Gets or sets the source job id for this client message.
+        /// </summary>
+        /// <value>
+        /// The source job id.
+        /// </value>
+        public override ulong SourceJobID
+        {
+            get { return Header.SourceJobID; }
+            set { Header.SourceJobID = value; }
+        }
+
+
+        /// <summary>
+        /// Gets the body structure of this message.
+        /// </summary>
+        public BodyType Body { get; private set; }
+
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ClientMsg&lt;BodyType&gt;"/> class.
+        /// This is a client send constructor.
+        /// </summary>
+        /// <param name="payloadReserve">The number of bytes to initialize the payload capacity to.</param>
+        public ClientGCMsg( int payloadReserve = 64 )
+            : base( payloadReserve )
+        {
+            Body = new BodyType();
+
+            // assign our emsg
+            msgType = Body.GetEMsg();
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ClientMsg&lt;BodyType&gt;"/> class.
+        /// This a reply constructor.
+        /// </summary>
+        /// <param name="msg">The message that this instance is a reply for.</param>
+        /// <param name="payloadReserve">The number of bytes to initialize the payload capacity to.</param>
+        public ClientGCMsg( GCMsgBase<MsgGCHdr> msg, int payloadReserve = 64 )
+            : this( payloadReserve )
+        {
+            // our target is where the message came from
+            Header.TargetJobID = msg.Header.SourceJobID;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ClientMsg&lt;BodyType&gt;"/> class.
+        /// This is a recieve constructor.
+        /// </summary>
+        /// <param name="msg">The packet message to build this client message from.</param>
+        public ClientGCMsg( IPacketMsg msg )
+            : this()
+        {
+            Deserialize( msg.GetData() );
+        }
+
+        /// <summary>
+        /// Serializes this client message instance to a byte array.
+        /// </summary>
+        /// <returns>
+        /// Data representing a client message.
+        /// </returns>
+        public override byte[] Serialize()
+        {
+            using ( MemoryStream ms = new MemoryStream() )
+            {
+                Header.Serialize( ms );
+                Body.Serialize( ms );
+                Payload.WriteTo( ms );
+
+                return ms.ToArray();
+            }
+        }
+        /// <summary>
+        /// Initializes this client message by deserializing the specified data.
+        /// </summary>
+        /// <param name="data">The data representing a client message.</param>
+        public override void Deserialize( byte[] data )
+        {
+            using ( MemoryStream ms = new MemoryStream( data ) )
+            {
+                Header.Deserialize( ms );
+                Body.Deserialize( ms );
+
+                // the rest of the data is the payload
+                int payloadOffset = ( int )ms.Position;
+                int payloadLen = ( int )( ms.Length - ms.Position );
+
+                Payload.Write( data, payloadOffset, payloadLen );
+            }
+        }
     }
 }
