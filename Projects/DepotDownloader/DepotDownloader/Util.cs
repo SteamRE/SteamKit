c6cfc52346ab42fc150c00cdf5c8e0cca04de997
psychonic <nshastings@gmail.com>
2012-06-05 20:51:28 -0400

    Backed out changeset: 26ba29eb818f

--- Projects/DepotDownloader/DepotDownloader/Util.cs
+++ Projects/DepotDownloader/DepotDownloader/Util.cs
@@ -11,6 +11,49 @@ namespace DepotDownloader
 {
     static class Util
     {
+        [DllImport( "libc" )]
+        static extern int uname( IntPtr buf );
+
+        static int _isMacOSX = -1;
+
+        // Environment.OSVersion.Platform returns PlatformID.Unix under Mono on OS X
+        // Code adapted from Mono: mcs/class/Managed.Windows.Forms/System.Windows.Forms/XplatUI.cs
+        public static bool IsMacOSX()
+        {
+            if ( _isMacOSX != -1 )
+                return _isMacOSX == 1;
+
+            IntPtr buf = IntPtr.Zero;
+
+            try
+            {
+                // The size of the utsname struct varies from system to system, but this _seems_ more than enough
+                buf = Marshal.AllocHGlobal( 4096 );
+
+                if ( uname( buf ) == 0 )
+                {
+                    string sys = Marshal.PtrToStringAnsi( buf );
+                    if ( sys == "Darwin" )
+                    {
+                        _isMacOSX = 1;
+                        return true;
+                    }
+                }
+            }
+            catch
+            {
+                // Do nothing?
+            }
+            finally
+            {
+                if ( buf != IntPtr.Zero )
+                    Marshal.FreeHGlobal( buf );
+            }
+
+            _isMacOSX = 0;
+            return false;
+        }
+
         public static string ReadPassword()
         {
             ConsoleKeyInfo keyInfo;
