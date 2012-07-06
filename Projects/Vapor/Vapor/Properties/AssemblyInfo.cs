ec8c576b4748f0c0c1286310fc4a71ea8f89013b
Ryan Stecker <voidedweasel@gmail.com>
2012-07-05 19:20:37 -0500

    Bumped up assembly version.

--- Projects/Vapor/Vapor/Properties/AssemblyInfo.cs
+++ Projects/Vapor/Vapor/Properties/AssemblyInfo.cs
@@ -37,4 +37,4 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion( "1.5.*" )]
+[assembly: AssemblyVersion( "1.6.*" )]
