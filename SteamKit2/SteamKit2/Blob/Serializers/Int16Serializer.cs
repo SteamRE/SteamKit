5ba243f489292ea6483e8b5154420f1b355ae4b2
Ryan Kistner <azuisleet@gmail.com>
2012-08-11 12:59:50 -0600

    BlobReader: type serializers for complete blob

---
+++ SteamKit2/SteamKit2/Blob/Serializers/Int16Serializer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace SteamKit2.Blob
+{
+    class Int16Serializer : IBlobSerializer
+    {
+        public Type ExpectedType
+        {
+            get
+            {
+                return typeof(Int16);
+            }
+        }
+
+        public object Read(Object target, BlobReader reader)
+        {
+            return reader.ReadFieldStream().ReadInt16();
+        }
+
+        public void EmitRead(JITContext context)
+        {
+            context.ReadFieldStream();
+            context.EmitStreamCall("ReadInt16");
+        }
+    }
+
+    class UInt16Serializer : IBlobSerializer
+    {
+        public Type ExpectedType
+        {
+            get
+            {
+                return typeof(UInt16);
+            }
+        }
+
+        public object Read(Object target, BlobReader reader)
+        {
+            return reader.ReadFieldStream().ReadUInt16();
+        }
+
+        public void EmitRead(JITContext context)
+        {
+            context.ReadFieldStream();
+            context.EmitStreamCall("ReadUInt16");
+        }
+    }
+}
---
+++ SteamKit2/SteamKit2/Blob/Serializers/MicroTimeSerializer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SteamKit2.Blob
+{
+    class MicroTimeSerializer : IBlobSerializer
+    {
+        public Type ExpectedType
+        {
+            get
+            {
+                return typeof(MicroTime);
+            }
+        }
+
+        public object Read(Object target, BlobReader reader)
+        {
+            return new MicroTime(reader.ReadFieldStream().ReadUInt64());
+        }
+
+        static Type[] CtorParams = new Type[] { typeof(ulong) };
+
+        public void EmitRead(JITContext context)
+        {
+            context.ReadFieldStream();
+            context.EmitStreamCall("ReadUInt64");
+            context.CreateType(typeof(MicroTime), CtorParams);
+        }
+    }
+}
