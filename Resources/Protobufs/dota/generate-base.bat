6aa71b9b41861983f0a3ddc480348bd2c640e540
Ryan Stecker <voidedweasel@gmail.com>
2012-10-10 23:42:08 -0500

    Updated generation script and generated a previously missing proto file.

--- Resources/Protobufs/dota/generate-base.bat
+++ Resources/Protobufs/dota/generate-base.bat
@@ -4,6 +4,7 @@ rem we use dota as the GC message base since it's the most updated
 
 echo Building GC base
 ..\..\Protogen\protogen -s:..\ -i:"steammessages.proto" -o:"..\..\..\SteamKit2\SteamKit2\Base\Generated\GC\SteamMsgBase.cs" -t:csharp -ns:"SteamKit2.GC.Internal"
+..\..\Protogen\protogen -s:..\ -i:"gcsystemmsgs.proto" -o:"..\..\..\SteamKit2\SteamKit2\Base\Generated\GC\SteamMsgGCSystem.cs" -t:csharp -ns:"SteamKit2.GC.Internal"
 ..\..\Protogen\protogen -s:..\ -i:"base_gcmessages.proto" -o:"..\..\..\SteamKit2\SteamKit2\Base\Generated\GC\SteamMsgGC.cs" -t:csharp -ns:"SteamKit2.GC.Internal"
 ..\..\Protogen\protogen -s:..\ -i:"gcsdk_gcmessages.proto" -o:"..\..\..\SteamKit2\SteamKit2\Base\Generated\GC\SteamMsgGCSDK.cs" -t:csharp -ns:"SteamKit2.GC.Internal"
 
