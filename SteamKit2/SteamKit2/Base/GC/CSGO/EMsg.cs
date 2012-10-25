2baa6b93074c795038324b0742a46d7ba571ce92
Didrole <didrole@gmail.com>
2012-10-25 18:38:43 +0200

    Updated CS:GO EMsg.

--- SteamKit2/SteamKit2/Base/GC/CSGO/EMsg.cs
+++ SteamKit2/SteamKit2/Base/GC/CSGO/EMsg.cs
@@ -6,8 +6,17 @@ namespace SteamKit2.GC.CSGO
 {
     public sealed class EGCMsg
     {
-        public const uint RequestEloBracket = 9010;
-        public const uint RequestEloBracketResponse = 9011;
+        public const uint StartMatchmakingDeprecated = 9001;
+        public const uint StartMatchmakingResponseDeprecated = 9002;
+        public const uint ReportRoundEndDeprecated = 9003;
+        public const uint StartMatchmaking = 9004;
+        public const uint StartMatchmakingResponse = 9005;
+        public const uint ReportRoundEnd = 9006;
+        public const uint ReportRoundEndLog = 9007;
+        public const uint RequestEloBrackets = 9008;
+        public const uint RequestEloBracketsResponse = 9009;
+        public const uint ClientRequestEloBracket = 9010;
+        public const uint ClientRequestEloBracketResponse = 9011;
     }
 }
 
