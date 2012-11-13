0b2dd0ab629f5dc0368f8b0964f98b116df976ba
Ryan Stecker <voidedweasel@gmail.com>
2012-11-12 21:07:19 -0600

    Add SteamKit2 sample projects.

---
+++ Samples/2.CallbackManager/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using SteamKit2;
+
+namespace Sample2_CallbackManager
+{
+    class Program
+    {
+        static SteamClient steamClient;
+        static CallbackManager manager;
+
+        static SteamUser steamUser;
+
+        static bool isRunning;
+
+        static string user, pass;
+
+
+        static void Main( string[] args )
+        {
+            if ( args.Length < 2 )
+            {
+                Console.WriteLine( "Sample2: No username and password specified!" );
+                return;
+            }
+
+            // save our logon details
+            user = args[ 0 ];
+            pass = args[ 1 ];
+
+            // create our steamclient instance
+            steamClient = new SteamClient();
+            // create the callback manager which will route callbacks to function calls
+            manager = new CallbackManager( steamClient );
+
+            // get the steamuser handler, which is used for logging on after successfully connecting
+            steamUser = steamClient.GetHandler<SteamUser>();
+
+            // register a few callbacks we're interested in
+            // these are registered upon creation to a callback manager, which will then route the callbacks
+            // to the functions specified
+            new Callback<SteamClient.ConnectedCallback>( OnConnected, manager );
+            new Callback<SteamClient.DisconnectedCallback>( OnDisconnected, manager );
+
+            new Callback<SteamUser.LoggedOnCallback>( OnLoggedOn, manager );
+            new Callback<SteamUser.LoggedOffCallback>( OnLoggedOff, manager );
+
+            isRunning = true;
+
+            Console.WriteLine( "Connecting to Steam..." );
+
+            // initiate the connection
+            steamClient.Connect();
+
+            // create our callback handling loop
+            while ( isRunning )
+            {
+                // in order for the callbacks to get routed, they need to be handled by the manager
+                manager.RunWaitCallbacks( TimeSpan.FromSeconds( 1 ) );
+            }
+        }
+
+        static void OnConnected( SteamClient.ConnectedCallback callback )
+        {
+            if ( callback.Result != EResult.OK )
+            {
+                Console.WriteLine( "Unable to connect to Steam: {0}", callback.Result );
+
+                isRunning = false;
+                return;
+            }
+
+            Console.WriteLine( "Connected to Steam! Logging in '{0}'...", user );
+
+            steamUser.LogOn( new SteamUser.LogOnDetails
+            {
+                Username = user,
+                Password = pass,
+            } );
+        }
+
+        static void OnDisconnected( SteamClient.DisconnectedCallback callback )
+        {
+            Console.WriteLine( "Disconnected from Steam" );
+
+            isRunning = false;
+        }
+
+        static void OnLoggedOn( SteamUser.LoggedOnCallback callback )
+        {
+            if ( callback.Result != EResult.OK )
+            {
+                Console.WriteLine( "Unable to logon to Steam: {0} / {1}", callback.Result, callback.ExtendedResult );
+
+                isRunning = false;
+                return;
+            }
+
+            Console.WriteLine( "Successfully logged on!" );
+
+            // at this point, we'd be able to perform actions on Steam
+            // such as go online on friends, send chat messages, join chat rooms, request app info, etc
+
+            // for this sample we'll just log off
+            steamUser.LogOff();
+        }
+
+        static void OnLoggedOff( SteamUser.LoggedOffCallback callback )
+        {
+            Console.WriteLine( "Logged off of Steam: {0}", callback.Result );
+        }
+    }
+}
