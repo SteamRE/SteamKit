c602ca89393693b35f556bc76c9c413cd58ae4f9
Ryan Stecker <voidedweasel@gmail.com>
2012-12-08 11:12:40 -0600

    Add EChatPermission. Closes #42.

--- SteamKit2/SteamKit2/Base/Generated/SteamLanguage.cs
+++ SteamKit2/SteamKit2/Base/Generated/SteamLanguage.cs
@@ -1456,6 +1456,24 @@ namespace SteamKit2
 		Member = 4,
 		Moderator = 8,
 	}
+	public enum EChatPermission
+	{
+		Close = 1,
+		Invite = 2,
+		Talk = 8,
+		Kick = 16,
+		Mute = 32,
+		SetMetadata = 64,
+		ChangePermissions = 128,
+		Ban = 256,
+		ChangeAccess = 512,
+		EveryoneNotInClanDefault = 8,
+		EveryoneDefault = 10,
+		MemberDefault = 282,
+		OfficerDefault = 282,
+		OwnerDefault = 891,
+		Mask = 1019,
+	}
 	[Flags]
 	public enum EFriendFlags
 	{
