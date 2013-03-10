0a196696ec9d4f6029b18d522fa5520d030a5a53
Netshroud <netshroud@gmail.com>
2013-03-10 20:26:35 +1100

    Added support for Steam Unified Messages

--- SteamKit2/SteamKit2/Steam3/SteamClient/SteamClient.cs
+++ SteamKit2/SteamKit2/Steam3/SteamClient/SteamClient.cs
@@ -63,6 +63,7 @@ namespace SteamKit2
             this.AddHandler( new SteamCloud() );
             this.AddHandler( new SteamWorkshop() );
             this.AddHandler( new SteamTrading() );
+            this.AddHandler( new SteamUnifiedMessages() );
         }
 
 
