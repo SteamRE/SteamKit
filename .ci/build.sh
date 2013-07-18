a4bc591e3067cff3f7e22fa0264d8dd1a8b43f11
Netshroud <netshroud@gmail.com>
2013-07-18 22:12:39 +1000

    Added samples and nethook analyzer to CI build

--- .ci/build.sh
+++ .ci/build.sh
@@ -1,5 +1,15 @@
 #!/bin/sh -x
 
 xbuild SteamKit2/SteamKit2.sln /target:SteamKit2 /target:Tests
+BUILDS[0]=$?
 
+xbuild Samples/Samples.sln /target:Sample1_Logon /target:Sample2_CallbackManager /target:Sample3_Extending /target:Sample4_DebugLog /target:Sample5_Friends /target:Sample6_SteamGuard
+BUILDS[1]=$?
+
+# These projects outcomes don't affect the build outcome
+xbuild Resources/NetHookAnalyzer/NetHookAnalyzer.sln /target:NetHookAnalyzer
+xbuild SteamKit2/SteamKit2.sln /target:SteamLanguageParser
+
+# Test to see if string.Join(" ", builds) contains anything other than 0 and space
+[[ ${BUILDS[@]} =~ ^[0\ ]*$ ]];
 exit $?
