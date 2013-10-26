f7427715e34649692a997e7de64f4e846d78421b
Ryan Stecker <voidedweasel@gmail.com>
2013-10-26 17:18:16 -0500

    Update assembly info.

--- SteamKit2/SteamKit2/Properties/AssemblyInfo.cs
+++ SteamKit2/SteamKit2/Properties/AssemblyInfo.cs
@@ -39,7 +39,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion( "1.4.1.*" )]
+[assembly: AssemblyVersion( "1.5.0.*" )]
 
 #if DEBUG
 [assembly: InternalsVisibleTo( "Tests" )]
