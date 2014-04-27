7d7f792a9a565be15fe25b0e99a8d0d8fd908b99
Netshroud <netshroud@gmail.com>
2014-04-27 22:26:01 +1000

    Include TF2's base_gcmessages since it's not in sync with Dota 2

--- Resources/Protobufs/tf/generate-base.bat
+++ Resources/Protobufs/tf/generate-base.bat
@@ -2,5 +2,9 @@
 
 rem the base GC messages are generated from the dota protobufs
 
+echo Building TF2 Base GC messages
+rem Remove when TF2 syncs up to with Dota/CS:GO again
+..\..\Protogen\protogen -s:..\ -i:"base_gcmessages.proto" -o:"..\..\..\SteamKit2\SteamKit2\Base\Generated\GC\TF2\SteamMsgBase.cs" -t:csharp -ns:"SteamKit2.GC.TF2.Internal"
+
 echo Building TF2 GC messages
 ..\..\Protogen\protogen -s:..\ -i:"tf_gcmessages.proto" -o:"..\..\..\SteamKit2\SteamKit2\Base\Generated\GC\TF2\MsgGC.cs" -t:csharp -ns:"SteamKit2.GC.TF2.Internal"
