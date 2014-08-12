a0239efe1c1d79d309107ffdc9ff983946849bfd
Christian Stewart <kidovate@gmail.com>
2014-08-11 18:37:33 -0700

    Basis of Dota 2 handlers

---
+++ SteamKit2/SteamKit2/Dota/Handlers/DotaLobbies/Callbacks.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * This file is subject to the terms and conditions defined in
+ * file 'license.txt', which is part of this source code package.
+ */
+
+
+
+using System;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using SteamKit2.Internal;
+
+namespace SteamKit2
+{
+	public partial class DotaLobbies
+	{
+	}
+}
---
+++ SteamKit2/SteamKit2/Dota/Handlers/DotaLobbies/DotaLobbies.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * This file is subject to the terms and conditions defined in
+ * file 'license.txt', which is part of this source code package.
+ */
+
+
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SteamKit2.Internal;
+
+namespace SteamKit2
+{
+	/// <summary>
+	/// This handler handles all Dota 2 GC lobbies interaction.
+	/// </summary>
+	public sealed partial class DotaLobbies : ClientMsgHandler
+	{
+		internal DotaLobbies()
+		{
+		}
+	}
+}
