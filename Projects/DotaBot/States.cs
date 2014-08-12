9be9e60a7b57295eff98ae7e253ae556008c5f99
Christian Stewart <kidovate@gmail.com>
2014-08-12 15:54:59 -0700

    Forgot indentation

--- Projects/DotaBot/States.cs
+++ Projects/DotaBot/States.cs
@@ -1,8 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// File Name:      States.cs
+// Project:           DotaBot
+// Copyright (c) christian stewart 2014
+// 
+// All rights reserved.
 
 namespace DotaBot
 {
@@ -13,16 +13,23 @@ namespace DotaBot
         Connected,
         DisconnectNoRetry,
         DisconnectRetry,
+
         #region DOTA
+
         Dota,
         DotaConnect,
         DotaMenu,
+
         #region DOTAJOIN
+
         DotaJoinLobby,
         DotaJoinFind,
         DotaJoinEnter,
+
         #endregion
+
         DotaLobby
+
         #endregion
     }
 }
