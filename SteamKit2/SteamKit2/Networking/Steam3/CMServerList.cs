e3bf17957ba2ec60796f51699f4152d7adc9c6e9
Netshroud <netshroud@gmail.com>
2015-05-30 20:37:39 +1000

    Lock the cm server list for thread safety, rename weighting to score, make good/bad multipliers instead of fixed values

--- SteamKit2/SteamKit2/Networking/Steam3/CMServerList.cs
+++ SteamKit2/SteamKit2/Networking/Steam3/CMServerList.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Net;
-using System.Text;
 
 namespace SteamKit2.Networking.Steam3
 {
@@ -12,11 +10,6 @@ namespace SteamKit2.Networking.Steam3
     /// </summary>
     public enum ServerQuality
     {
-        /// <summary>
-        /// No quality marked, or validity period expired.
-        /// </summary>
-        Undetermined = 0,
-
         /// <summary>
         /// Known good server.
         /// </summary>
@@ -27,40 +20,43 @@ namespace SteamKit2.Networking.Steam3
         /// </summary>
         Bad
     };
-
     /// <summary>
     /// Smart list of CM servers.
     /// </summary>
     public class SmartCMServerList
     {
-        const int BaseWeighting = 100;
-        const int GoodWeighting = 450;
-        const int BadWeighting = 15;
+        const int BaseScore = 1000;
+        const float GoodWeighting = 1.5f;
+        const float BadWeighting = 0.6f;
         
-        [System.Diagnostics.DebuggerDisplay("ServerInfo ({EndPoint}, Weighting {Weighting})")]
+        [System.Diagnostics.DebuggerDisplay("ServerInfo ({EndPoint}, Score {Score})")]
         class ServerInfo
         {
             public IPEndPoint EndPoint { get; set; }
-            public int Weighting { get; set; }
-            public DateTime LastWeightingChangeTimeUtc { get; set; }
+            public int Score { get; set; }
+            public DateTime LastScoreChangeTimeUtc { get; set; }
         }
 
         internal SmartCMServerList()
         {
             servers = new Collection<ServerInfo>();
-            weightingValidityTimeSpan = TimeSpan.FromMinutes(5);
+            scoreExpiryTimeSpan = TimeSpan.FromMinutes(5);
         }
 
         Collection<ServerInfo> servers;
-        TimeSpan weightingValidityTimeSpan;
+        TimeSpan scoreExpiryTimeSpan;
+        object listLock = new object();
 
-        void ResetOldWeightings()
+        public void ResetOldScores()
         {
-            foreach(var serverInfo in servers)
+            lock (listLock)
             {
-                if (serverInfo.LastWeightingChangeTimeUtc + weightingValidityTimeSpan <= DateTime.UtcNow)
+                foreach (var serverInfo in servers)
                 {
-                    serverInfo.Weighting = BaseWeighting;
+                    if (serverInfo.LastScoreChangeTimeUtc + scoreExpiryTimeSpan <= DateTime.UtcNow)
+                    {
+                        serverInfo.Score = BaseScore;
+                    }
                 }
             }
         }
@@ -72,18 +68,21 @@ namespace SteamKit2.Networking.Steam3
         /// <returns>false if the server is already in the list, true otherwise.</returns>
         public bool TryAdd(IPEndPoint endPoint)
         {
-            if (servers.Where(x => x.EndPoint == endPoint).Any())
+            lock (listLock)
             {
-                return false;
-            }
+                if (servers.Any(x => x.EndPoint == endPoint))
+                {
+                    return false;
+                }
 
-            AddCore(endPoint);
+                AddCore(endPoint);
+            }
             return true;
         }
 
         void Add(IPEndPoint endPoint)
         {
-            if (servers.Where(x => x.EndPoint == endPoint).Any())
+            if (servers.Any(x => x.EndPoint == endPoint))
             {
                 throw new ArgumentException("The supplied endpoint is already in the server list.", "endPoint");
             }
@@ -96,8 +95,8 @@ namespace SteamKit2.Networking.Steam3
             var info = new ServerInfo
             {
                 EndPoint = endPoint,
-                Weighting = BaseWeighting,
-                LastWeightingChangeTimeUtc = DateTime.UtcNow,
+                Score = BaseScore,
+                LastScoreChangeTimeUtc = DateTime.UtcNow,
             };
 
             servers.Add(info);
@@ -106,11 +105,14 @@ namespace SteamKit2.Networking.Steam3
         /// <summary>
         /// Explicitly resets the quality of every stored server.
         /// </summary>
-        public void ResetWeightings()
+        public void ResetAllScores()
         {
-            foreach(var server in servers)
+            lock (listLock)
             {
-                SetServerQuality(server, ServerQuality.Undetermined);
+                foreach (var server in servers)
+                {
+                    SetServerScore(server, BaseScore);
+                }
             }
         }
 
@@ -124,41 +126,44 @@ namespace SteamKit2.Networking.Steam3
         /// </summary>
         public void UseInbuiltList()
         {
-            Clear();
-            Add(new IPEndPoint( IPAddress.Parse( "208.64.200.201" ), 27017 ));
-            Add(new IPEndPoint( IPAddress.Parse( "208.64.200.201" ), 27018 ));
-            Add(new IPEndPoint( IPAddress.Parse( "208.64.200.201" ), 27019 ));
-            Add(new IPEndPoint( IPAddress.Parse( "208.64.200.201" ), 27020 ));
-            Add(new IPEndPoint( IPAddress.Parse( "208.64.200.202" ), 27017 ));
-            Add(new IPEndPoint( IPAddress.Parse( "208.64.200.202" ), 27018 ));
-            Add(new IPEndPoint( IPAddress.Parse( "208.64.200.202" ), 27019 ));
-            Add(new IPEndPoint( IPAddress.Parse( "208.64.200.203" ), 27017 ));
-            Add(new IPEndPoint( IPAddress.Parse( "208.64.200.203" ), 27018 ));
-            Add(new IPEndPoint( IPAddress.Parse( "208.64.200.203" ), 27019 ));
-            Add(new IPEndPoint( IPAddress.Parse( "208.64.200.204" ), 27017 ));
-            Add(new IPEndPoint( IPAddress.Parse( "208.64.200.204" ), 27018 ));
-            Add(new IPEndPoint( IPAddress.Parse( "208.64.200.204" ), 27019 ));
-            Add(new IPEndPoint( IPAddress.Parse( "208.64.200.205" ), 27017 ));
-            Add(new IPEndPoint( IPAddress.Parse( "208.64.200.205" ), 27018 ));
-            Add(new IPEndPoint( IPAddress.Parse( "208.64.200.205" ), 27019 ));
-            Add(new IPEndPoint( IPAddress.Parse( "208.78.164.9" ), 27017 ));
-            Add(new IPEndPoint( IPAddress.Parse( "208.78.164.9" ), 27018 ));
-            Add(new IPEndPoint( IPAddress.Parse( "208.78.164.9" ), 27019 ));
-            Add(new IPEndPoint( IPAddress.Parse( "208.78.164.10" ), 27017 ));
-            Add(new IPEndPoint( IPAddress.Parse( "208.78.164.10" ), 27018 ));
-            Add(new IPEndPoint( IPAddress.Parse( "208.78.164.10" ), 27019 ));
-            Add(new IPEndPoint( IPAddress.Parse( "208.78.164.11" ), 27017 ));
-            Add(new IPEndPoint( IPAddress.Parse( "208.78.164.11" ), 27018 ));
-            Add(new IPEndPoint( IPAddress.Parse( "208.78.164.11" ), 27019 ));
-            Add(new IPEndPoint( IPAddress.Parse( "208.78.164.12" ), 27017 ));
-            Add(new IPEndPoint( IPAddress.Parse( "208.78.164.12" ), 27018 ));
-            Add(new IPEndPoint( IPAddress.Parse( "208.78.164.12" ), 27019 ));
-            Add(new IPEndPoint( IPAddress.Parse( "208.78.164.13" ), 27017 ));
-            Add(new IPEndPoint( IPAddress.Parse( "208.78.164.13" ), 27018 ));
-            Add(new IPEndPoint( IPAddress.Parse( "208.78.164.13" ), 27019 ));
-            Add(new IPEndPoint( IPAddress.Parse( "208.78.164.14" ), 27017 ));
-            Add(new IPEndPoint( IPAddress.Parse( "208.78.164.14" ), 27018 ));
-            Add(new IPEndPoint( IPAddress.Parse( "208.78.164.14" ), 27019 ));
+            lock (listLock)
+            {
+                Clear();
+                Add(new IPEndPoint(IPAddress.Parse("208.64.200.201"), 27017));
+                Add(new IPEndPoint(IPAddress.Parse("208.64.200.201"), 27018));
+                Add(new IPEndPoint(IPAddress.Parse("208.64.200.201"), 27019));
+                Add(new IPEndPoint(IPAddress.Parse("208.64.200.201"), 27020));
+                Add(new IPEndPoint(IPAddress.Parse("208.64.200.202"), 27017));
+                Add(new IPEndPoint(IPAddress.Parse("208.64.200.202"), 27018));
+                Add(new IPEndPoint(IPAddress.Parse("208.64.200.202"), 27019));
+                Add(new IPEndPoint(IPAddress.Parse("208.64.200.203"), 27017));
+                Add(new IPEndPoint(IPAddress.Parse("208.64.200.203"), 27018));
+                Add(new IPEndPoint(IPAddress.Parse("208.64.200.203"), 27019));
+                Add(new IPEndPoint(IPAddress.Parse("208.64.200.204"), 27017));
+                Add(new IPEndPoint(IPAddress.Parse("208.64.200.204"), 27018));
+                Add(new IPEndPoint(IPAddress.Parse("208.64.200.204"), 27019));
+                Add(new IPEndPoint(IPAddress.Parse("208.64.200.205"), 27017));
+                Add(new IPEndPoint(IPAddress.Parse("208.64.200.205"), 27018));
+                Add(new IPEndPoint(IPAddress.Parse("208.64.200.205"), 27019));
+                Add(new IPEndPoint(IPAddress.Parse("208.78.164.9"), 27017));
+                Add(new IPEndPoint(IPAddress.Parse("208.78.164.9"), 27018));
+                Add(new IPEndPoint(IPAddress.Parse("208.78.164.9"), 27019));
+                Add(new IPEndPoint(IPAddress.Parse("208.78.164.10"), 27017));
+                Add(new IPEndPoint(IPAddress.Parse("208.78.164.10"), 27018));
+                Add(new IPEndPoint(IPAddress.Parse("208.78.164.10"), 27019));
+                Add(new IPEndPoint(IPAddress.Parse("208.78.164.11"), 27017));
+                Add(new IPEndPoint(IPAddress.Parse("208.78.164.11"), 27018));
+                Add(new IPEndPoint(IPAddress.Parse("208.78.164.11"), 27019));
+                Add(new IPEndPoint(IPAddress.Parse("208.78.164.12"), 27017));
+                Add(new IPEndPoint(IPAddress.Parse("208.78.164.12"), 27018));
+                Add(new IPEndPoint(IPAddress.Parse("208.78.164.12"), 27019));
+                Add(new IPEndPoint(IPAddress.Parse("208.78.164.13"), 27017));
+                Add(new IPEndPoint(IPAddress.Parse("208.78.164.13"), 27018));
+                Add(new IPEndPoint(IPAddress.Parse("208.78.164.13"), 27019));
+                Add(new IPEndPoint(IPAddress.Parse("208.78.164.14"), 27017));
+                Add(new IPEndPoint(IPAddress.Parse("208.78.164.14"), 27018));
+                Add(new IPEndPoint(IPAddress.Parse("208.78.164.14"), 27019));
+            }
         }
 
         /// <summary>
@@ -169,37 +174,34 @@ namespace SteamKit2.Networking.Steam3
         /// <exception cref="System.ArgumentException">The supplied endPoint does not represent a server in the list.</exception>
         public void Mark(IPEndPoint endPoint, ServerQuality quality)
         {
-            var serverInfo = servers.Where(x => x.EndPoint.Equals(endPoint)).SingleOrDefault();
-            if (serverInfo == null)
+            lock (listLock)
             {
-                throw new ArgumentException("The supplied endpoint is not in the server list.", "endPoint");
-            }
+                var serverInfo = servers.Where(x => x.EndPoint.Equals(endPoint)).SingleOrDefault();
+                if (serverInfo == null)
+                {
+                    throw new ArgumentException("The supplied endpoint is not in the server list.", "endPoint");
+                }
 
-            SetServerQuality(serverInfo, quality);
-        }
+                switch (quality)
+                {
+                    case ServerQuality.Good:
+                        SetServerScore(serverInfo, Convert.ToInt32(serverInfo.Score * GoodWeighting));
+                        break;
 
-        static int GetWeighting(ServerQuality quality)
-        {
-            switch (quality)
-            {
-                case ServerQuality.Bad:
-                    return BadWeighting;
+                    case ServerQuality.Bad:
+                        SetServerScore(serverInfo, Convert.ToInt32(serverInfo.Score * BadWeighting));
+                        break;
 
-                case ServerQuality.Good:
-                    return GoodWeighting;
-
-                case ServerQuality.Undetermined:
-                    return BaseWeighting;
-
-                default:
-                    throw new ArgumentOutOfRangeException("quality");
+                    default:
+                        throw new ArgumentOutOfRangeException("quality");
+                }
             }
         }
 
-        void SetServerQuality(ServerInfo serverInfo, ServerQuality quality)
+        void SetServerScore(ServerInfo serverInfo, int score)
         {
-            serverInfo.Weighting = GetWeighting(quality);
-            serverInfo.LastWeightingChangeTimeUtc = DateTime.UtcNow;
+            serverInfo.Score = score;
+            serverInfo.LastScoreChangeTimeUtc = DateTime.UtcNow;
         }
 
         /// <summary>
@@ -208,18 +210,24 @@ namespace SteamKit2.Networking.Steam3
         /// <returns>An <see cref="System.Net.IPEndPoint"/>, or null if the list is empty.</returns>
         public IPEndPoint GetNextServer()
         {
-            ResetOldWeightings();
-
-            var totalWeightingValue = servers.Sum(x => x.Weighting);
-            var randomValue = new Random().Next(totalWeightingValue);
-
-            var weightingMarker = 0;
-            foreach(var serverInfo in servers)
+            lock (listLock)
             {
-                weightingMarker += serverInfo.Weighting;
-                if (weightingMarker >= randomValue)
+                // ResetOldScores takes a lock internally, however
+                // locks are re-entrant on the same thread, so this
+                // isn't a problem.
+                ResetOldScores();
+
+                var totalScoreValue = servers.Sum(x => x.Score);
+                var randomValue = new Random().Next(totalScoreValue);
+
+                var scoreMarker = 0;
+                foreach (var serverInfo in servers)
                 {
-                    return serverInfo.EndPoint;
+                    scoreMarker += serverInfo.Score;
+                    if (scoreMarker >= randomValue)
+                    {
+                        return serverInfo.EndPoint;
+                    }
                 }
             }
 
