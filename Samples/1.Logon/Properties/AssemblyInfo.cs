bb7aa2a058127763d52b6c4460932e51cc1ce158
Netshroud <netshroud@gmail.com>
2015-06-20 19:37:48 +1000

    Missed a spot (#124)

--- Samples/1.Logon/Properties/AssemblyInfo.cs
+++ Samples/1.Logon/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SteamKit Sample 1: Logon")]
-[assembly: AssemblyDescription("SteamKit Sample 2: Logon")]
+[assembly: AssemblyDescription("SteamKit Sample 1: Logon")]
 [assembly: AssemblyConfiguration( "" )]
 [assembly: AssemblyCompany( "" )]
 [assembly: AssemblyProduct("Sample1_CallbackManager")]
