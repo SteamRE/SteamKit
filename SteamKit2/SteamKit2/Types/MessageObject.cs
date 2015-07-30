a37d949fa8fc6c07d85c25f43a1c25d0d3fac425
Ryan Stecker <ryan@stecker.email>
2015-07-30 12:14:35 -0500

    Make use of TryReadAsBinary.

--- SteamKit2/SteamKit2/Types/MessageObject.cs
+++ SteamKit2/SteamKit2/Types/MessageObject.cs
@@ -41,16 +41,7 @@ namespace SteamKit2
         /// <returns><c>true</c> on success; otherwise, <c>false</c>.</returns>
         public bool ReadFromStream( Stream stream )
         {
-            bool success = KeyValues.ReadAsBinary( stream );
-
-            if ( success )
-            {
-                // our binary KV parsing is a a little wonky in that we load the data one extra layer deep,
-                // so the actual MessageObject that we want is the first child
-                KeyValues = KeyValues.Children.FirstOrDefault() ?? KeyValue.Invalid;
-            }
-
-            return success;
+            return KeyValues.TryReadAsBinary( stream );
         }
     }
 }
