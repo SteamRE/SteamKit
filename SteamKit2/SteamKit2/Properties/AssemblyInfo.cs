506e176bd308a9f2ecd90ecff1f08bb73f0d5621
Netshroud <netshroud@gmail.com>
2015-08-03 18:00:52 +1000

    Bump version to 1.6.4, add release notes

--- SteamKit2/SteamKit2/Properties/AssemblyInfo.cs
+++ SteamKit2/SteamKit2/Properties/AssemblyInfo.cs
@@ -39,7 +39,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion( "1.6.3.*" )]
+[assembly: AssemblyVersion( "1.6.4.*" )]
 
 #if DEBUG
 [assembly: InternalsVisibleTo( "Tests" )]
