6275b446055b23083a47cc82cb64a54d50d2eb33
Netshroud <netshroud@gmail.com>
2015-08-26 21:15:21 +1000

    Implement OS X Machine ID info - dragons be here.

--- SteamKit2/SteamKit2/Util/HardwareUtils.cs
+++ SteamKit2/SteamKit2/Util/HardwareUtils.cs
@@ -6,6 +6,12 @@ using System.Management;
 using System.Net.NetworkInformation;
 using System.Text;
 using Microsoft.Win32;
+using SteamKit2.Util.MacHelpers;
+
+using static SteamKit2.Util.MacHelpers.LibC;
+using static SteamKit2.Util.MacHelpers.CoreFoundation;
+using static SteamKit2.Util.MacHelpers.DiskArbitration;
+using static SteamKit2.Util.MacHelpers.IOKit;
 
 namespace SteamKit2
 {
@@ -20,11 +26,14 @@ namespace SteamKit2
                     return new WindowsInfoProvider();
 
                 case PlatformID.Unix:
-                    if ( !Utils.IsRunningOnDarwin() )
+                    if ( Utils.IsRunningOnDarwin() )
+                    {
+                        return new OSXInfoProvider();
+                    }
+                    else
                     {
                         return new LinuxInfoProvider();
                     }
-                    break;
             }
 
             return new DefaultInfoProvider();
@@ -161,6 +170,64 @@ namespace SteamKit2
         public override byte[] GetDiskId()
         {
             // todo: need a not-so-painful way to get drive uuids
+            return base.GetDiskId();
+        }
+    }
+
+    class OSXInfoProvider : DefaultInfoProvider
+    {
+        public override byte[] GetMachineGuid()
+        {
+            uint platformExpert = IOServiceGetMatchingService( kIOMasterPortDefault, IOServiceMatching( "IOPlatformExpertDevice" ) );
+            if ( platformExpert != 0 )
+            {
+                try
+                {
+                    using ( var serialNumberKey = CFStringCreateWithCString( CFTypeRef.None, kIOPlatformSerialNumberKey, CFStringEncoding.kCFStringEncodingASCII ) )
+                    {
+                        var serialNumberAsString = IORegistryEntryCreateCFProperty( platformExpert, serialNumberKey, CFTypeRef.None, 0 );
+                        var sb = new StringBuilder( 64 );
+                        if ( CFStringGetCString( serialNumberAsString, sb, sb.Capacity, CFStringEncoding.kCFStringEncodingASCII ) )
+                        {
+                            return Encoding.ASCII.GetBytes( sb.ToString() );
+                        }
+                    }
+                }
+                finally
+                {
+                    IOKit.IOObjectRelease( platformExpert );
+                }
+            }
+
+            return base.GetMachineGuid();
+        }
+
+        public override byte[] GetDiskId()
+        {
+            var stat = new statfs();
+            var statted = statfs64( "/", ref stat );
+            if ( statted == 0 )
+            {
+                using ( var session = DASessionCreate( CFTypeRef.None ) )
+                using ( var disk = DADiskCreateFromBSDName( CFTypeRef.None, session, stat.f_mntfromname ) )
+                using ( var properties = DADiskCopyDescription( disk ) )
+                using ( var key = CFStringCreateWithCString( CFTypeRef.None, DiskArbitration.kDADiskDescriptionMediaUUIDKey, CFStringEncoding.kCFStringEncodingASCII ) )
+                {
+                    IntPtr cfuuid = IntPtr.Zero;
+                    if ( CFDictionaryGetValueIfPresent( properties, key, out cfuuid ) )
+                    {
+                        using ( var uuidString = CFUUIDCreateString( CFTypeRef.None, cfuuid ) )
+                        {
+                            var stringBuilder = new StringBuilder( 64 );
+                            if ( CFStringGetCString( uuidString, stringBuilder, stringBuilder.Capacity, CFStringEncoding.kCFStringEncodingASCII ) )
+                            {
+                                return Encoding.ASCII.GetBytes( stringBuilder.ToString() );
+                            }
+                        }
+                    }
+                }
+            }
+
             return base.GetDiskId();
         }
     }
---
+++ SteamKit2/SteamKit2/Util/MacHelpers.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace SteamKit2.Util.MacHelpers
+{
+    class CFTypeRef : SafeHandle
+    {
+        CFTypeRef()
+            : base(IntPtr.Zero, ownsHandle: true)
+        {
+        }
+
+        protected override bool ReleaseHandle()
+        {
+            if (IsInvalid)
+            {
+                return false;
+            }
+
+            CoreFoundation.CFRelease(handle);
+            return true;
+        }
+
+        public override bool IsInvalid
+        {
+            get { return handle == IntPtr.Zero; }
+        }
+
+        public static CFTypeRef None
+        {
+            get { return new CFTypeRef(); }
+        }
+    }
+
+    // Taken from <sys/mount.h>
+    struct statfs
+    {
+        const int MFSTYPENAMELEN = 16;
+        const int PATH_MAX = 1024;
+
+        public uint    f_bsize;    /* fundamental file system block size */
+        public int     f_iosize;   /* optimal transfer block size */
+        public ulong   f_blocks;   /* total data blocks in file system */
+        public ulong   f_bfree;    /* free blocks in fs */
+        public ulong   f_bavail;   /* free blocks avail to non-superuser */
+        public ulong   f_files;    /* total file nodes in file system */
+        public ulong   f_ffree;    /* free file nodes in fs */
+
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 2)]
+        public uint[]  f_fsid;     /* file system id */
+        public uint    f_owner;    /* user that mounted the filesystem */
+        public uint    f_type;     /* type of filesystem */
+        public uint    f_flags;    /* copy of mount exported flags */
+        public uint    f_fssubtype;    /* fs sub-type (flavor) */
+
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = MFSTYPENAMELEN)]
+        public string  f_fstypename;  /* fs type name */
+
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = PATH_MAX)]
+        public string  f_mntonname;    /* directory on which mounted */
+
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = PATH_MAX)]
+        public string  f_mntfromname ; /* mounted filesystem */
+
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 8)]
+        public uint[]  f_reserved;  /* For future use */
+    }
+
+    static class LibC
+    {
+        const string LibraryName = "libc";
+
+        [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern int statfs64(string path, ref statfs buf);
+    }
+
+    static class CoreFoundation
+    {
+        const string LibraryName = "CoreFoundation.framework/CoreFoundation";
+
+        public enum CFStringEncoding : uint
+        {
+            kCFStringEncodingASCII = 0x0600
+        }
+
+        [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void CFRelease(IntPtr cf);
+
+        [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl)]
+        [return: MarshalAs(UnmanagedType.I1)]
+        public static extern bool CFDictionaryGetValueIfPresent(CFTypeRef theDict, CFTypeRef key, out IntPtr value);
+
+        [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern CFTypeRef CFStringCreateWithCString(CFTypeRef allocator, string cStr, CFStringEncoding encoding);
+
+        [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl)]
+        [return:MarshalAs(UnmanagedType.U1)]
+        public static extern bool CFStringGetCString(CFTypeRef theString, StringBuilder buffer, long bufferSize, CFStringEncoding encoding);
+
+        [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern CFTypeRef CFUUIDCreateString(CFTypeRef allocator, IntPtr uuid);
+    }
+
+    static class DiskArbitration
+    {
+        const string LibraryName = "DiskArbitration.framework/DiskArbitration";
+        public const string kDADiskDescriptionMediaUUIDKey = "DAMediaUUID";
+
+        [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern CFTypeRef DASessionCreate(CFTypeRef allocator);
+
+        [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern CFTypeRef DADiskCreateFromBSDName(CFTypeRef allocator, CFTypeRef session, string name);
+
+        [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern CFTypeRef DADiskCopyDescription(CFTypeRef disk);
+    }
+
+    static class IOKit
+    {
+        const string LibraryName = "IOKit.framework/IOKit";
+
+        public const uint kIOMasterPortDefault = 0;
+        public const string kIOPlatformSerialNumberKey = "IOPlatformSerialNumber";
+
+        [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern CFTypeRef IORegistryEntryCreateCFProperty(uint entry, CFTypeRef key, CFTypeRef allocator, uint options);
+
+        [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern uint IOServiceGetMatchingService(uint masterPort, IntPtr matching);
+
+        [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr IOServiceMatching(string name);
+
+        [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern int IOObjectRelease(uint @object);
+    }
+}
+
