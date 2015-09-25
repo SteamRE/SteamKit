ef8f9ba3a029f530db2f495b3208ed11041055aa
Nicholas Hastings <nshastings@gmail.com>
2015-09-24 21:18:44 -0400

    Update NetHookAnalyzer2 to handle more SO GC message, with specialization.

---
+++ Resources/NetHookAnalyzer2/NetHookAnalyzer2/Specializations/Dota2SOHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SteamKit2.GC.Dota.Internal;
+using SteamKit2.GC.Internal;
+using System.Collections.ObjectModel;
+
+namespace NetHookAnalyzer2.Specializations
+{
+    static class Dota2SOHelper
+    {
+        public static Dictionary<int, Type> SOTypes = new Dictionary<int, Type>()
+        {
+            {1, typeof(CSOEconItem)},
+            {5, typeof(CSOItemRecipe)},
+            {7, typeof(CSOEconGameAccountClient)},
+            {35, typeof(CSOSelectedItemPreset)},
+            {36, typeof(CSOEconItemPresetInstance)},
+            {38, typeof(CSOEconItemDropRateBonus)},
+            {39, typeof(CSOEconItemLeagueViewPass)},
+            {40, typeof(CSOEconItemEventTicket)},
+            {42, typeof(CSOEconItemTournamentPassport)},
+            {2002, typeof(CSODOTAGameAccountClient)},
+            {2003, typeof(CSODOTAParty)},
+            {2004, typeof(CSODOTALobby)},
+            {2006, typeof(CSODOTAPartyInvite)},
+            {2007, typeof(CSODOTAGameHeroFavorites)},
+            {2008, typeof(CSODOTAMapLocationState)},
+            {2009, typeof(CMsgDOTATournament)},
+            {2010, typeof(CSODOTAPlayerChallenge)},
+            {2011, typeof(CSODOTALobbyInvite)},
+        };
+    }
+}
