754e28da64d806e8d647e9c3a7f9957c3ae3b58d
Pavel Djundik <github@xpaw.me>
2016-01-29 16:12:36 +0200

    Update GetNumberOfCurrentPlayers to use protobuf method

--- SteamKit2/SteamKit2/Steam/Handlers/SteamUserStats/SteamUserStats.cs
+++ SteamKit2/SteamKit2/Steam/Handlers/SteamUserStats/SteamUserStats.cs
@@ -22,7 +22,7 @@ namespace SteamKit2
         {
             dispatchMap = new Dictionary<EMsg, Action<IPacketMsg>>
             {
-                { EMsg.ClientGetNumberOfCurrentPlayersResponse, HandleNumberOfPlayersResponse },
+                { EMsg.ClientGetNumberOfCurrentPlayersDPResponse, HandleNumberOfPlayersResponse },
                 { EMsg.ClientLBSFindOrCreateLBResponse, HandleFindOrCreateLBResponse },
                 { EMsg.ClientLBSGetLBEntriesResponse, HandleGetLBEntriesRespons },
             };
@@ -30,17 +30,17 @@ namespace SteamKit2
 
 
         /// <summary>
-        /// Retrieves the number of current players for a given <see cref="GameID"/>.
+        /// Retrieves the number of current players for a given app id.
         /// Results are returned in a <see cref="NumberOfPlayersCallback"/>.
         /// </summary>
-        /// <param name="gameId">The GameID to request the number of players for.</param>
+        /// <param name="appId">The app id to request the number of players for.</param>
         /// <returns>The Job ID of the request. This can be used to find the appropriate <see cref="NumberOfPlayersCallback"/>.</returns>
-        public AsyncJob<NumberOfPlayersCallback> GetNumberOfCurrentPlayers( GameID gameId )
+        public AsyncJob<NumberOfPlayersCallback> GetNumberOfCurrentPlayers( uint appId )
         {
-            var msg = new ClientMsg<MsgClientGetNumberOfCurrentPlayers>();
+            var msg = new ClientMsgProtobuf<CMsgDPGetNumberOfCurrentPlayers>( EMsg.ClientGetNumberOfCurrentPlayersDP );
             msg.SourceJobID = Client.GetNextJobID();
 
-            msg.Body.GameID = gameId;
+            msg.Body.appid = appId;
 
             Client.Send( msg );
 
@@ -152,9 +152,9 @@ namespace SteamKit2
         #region ClientMsg Handlers
         void HandleNumberOfPlayersResponse( IPacketMsg packetMsg )
         {
-            var msg = new ClientMsg<MsgClientGetNumberOfCurrentPlayersResponse>( packetMsg );
+            var msg = new ClientMsgProtobuf<CMsgDPGetNumberOfCurrentPlayersResponse>( packetMsg );
 
-            var callback = new NumberOfPlayersCallback( msg.Header.TargetJobID, msg.Body );
+            var callback = new NumberOfPlayersCallback( msg.TargetJobID, msg.Body );
             Client.PostCallback( callback );
         }
         void HandleFindOrCreateLBResponse( IPacketMsg packetMsg )
