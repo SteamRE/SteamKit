9367a602eec59783ab8e4bb70b64aa985f629058
Nicholas Hastings <nshastings@gmail.com>
2016-02-21 11:34:26 -0500

    [NHA2] Add specializations for TF2 and CS:GO GC SO messages.

---
+++ Resources/NetHookAnalyzer2/NetHookAnalyzer2/Specializations/TF2/TF2SOHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using SteamKit2.GC.TF2.Internal;
+
+namespace NetHookAnalyzer2.Specializations
+{
+    class TF2SOHelper
+    {
+        public static Dictionary<int, Type> SOTypes = new Dictionary<int, Type>()
+        {
+            {1, typeof(CSOEconItem)},
+            {2, typeof(CSOTFPlayerInfo)},
+            {3, typeof(CSOEconClaimCode)},
+            {5, typeof(CSOItemRecipe)},
+            {7, typeof(CSOEconGameAccountClient)},
+            {19, typeof(CSOTFDuelSummary)},
+            {28, typeof(CSOTFMapContribution)},
+            {37, typeof(CSOEconGameAccountForGameServers)},
+            {39, typeof(CSOTFLadderPlayerStats)},
+            {1001, typeof(CSOPartyInvite)},
+            {2003, typeof(CSOTFParty)},
+            {2004, typeof(CSOTFLobby)},
+            {2006, typeof(CSOTFPartyInvite)},
+        };
+    }
+}
