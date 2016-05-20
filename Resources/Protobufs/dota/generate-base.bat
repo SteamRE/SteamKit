6b9df53889aa1e3c025ca614c34b14e4ab9ff54f
Nicholas Hastings <nshastings@gmail.com>
2016-05-20 08:05:10 -0400

    Add new files to Dota proto gen script.

--- Resources/Protobufs/dota/generate-base.bat
+++ Resources/Protobufs/dota/generate-base.bat
@@ -14,8 +14,17 @@ echo Building Steamworks Unified Messages
 echo Building Dota messages...
 ..\..\Protogen\protogen -s:..\ -i:"network_connection.proto" -o:"..\..\..\SteamKit2\SteamKit2\Base\Generated\GC\Dota\NetworkConnection.cs" -t:csharp -ns:"SteamKit2.GC.Dota.Internal"
 
+..\..\Protogen\protogen -s:..\ -i:"dota_client_enums.proto" -o:"..\..\..\SteamKit2\SteamKit2\Base\Generated\GC\Dota\MsgClientEnums.cs" -t:csharp -ns:"SteamKit2.GC.Dota.Internal"
 ..\..\Protogen\protogen -s:..\ -i:"dota_shared_enums.proto" -o:"..\..\..\SteamKit2\SteamKit2\Base\Generated\GC\Dota\MsgSharedEnums.cs" -t:csharp -ns:"SteamKit2.GC.Dota.Internal"
 ..\..\Protogen\protogen -s:..\ -i:"dota_gcmessages_common.proto" -o:"..\..\..\SteamKit2\SteamKit2\Base\Generated\GC\Dota\MsgGCCommon.cs" -t:csharp -ns:"SteamKit2.GC.Dota.Internal"
+..\..\Protogen\protogen -s:..\ -i:"dota_gcmessages_common_match_management.proto" -o:"..\..\..\SteamKit2\SteamKit2\Base\Generated\GC\Dota\MsgGCCommonMatchMgmt.cs" -t:csharp -ns:"SteamKit2.GC.Dota.Internal"
+..\..\Protogen\protogen -s:..\ -i:"dota_gcmessages_msgid.proto" -o:"..\..\..\SteamKit2\SteamKit2\Base\Generated\GC\Dota\MsgGCMsgId.cs" -t:csharp -ns:"SteamKit2.GC.Dota.Internal"
 ..\..\Protogen\protogen -s:..\ -i:"dota_gcmessages_client.proto" -o:"..\..\..\SteamKit2\SteamKit2\Base\Generated\GC\Dota\MsgGCClient.cs" -t:csharp -ns:"SteamKit2.GC.Dota.Internal"
+..\..\Protogen\protogen -s:..\ -i:"dota_gcmessages_client_chat.proto" -o:"..\..\..\SteamKit2\SteamKit2\Base\Generated\GC\Dota\MsgGCClientChat.cs" -t:csharp -ns:"SteamKit2.GC.Dota.Internal"
 ..\..\Protogen\protogen -s:..\ -i:"dota_gcmessages_client_fantasy.proto" -o:"..\..\..\SteamKit2\SteamKit2\Base\Generated\GC\Dota\MsgGCClientFantasy.cs" -t:csharp -ns:"SteamKit2.GC.Dota.Internal"
+..\..\Protogen\protogen -s:..\ -i:"dota_gcmessages_client_guild.proto" -o:"..\..\..\SteamKit2\SteamKit2\Base\Generated\GC\Dota\MsgGCClientGuild.cs" -t:csharp -ns:"SteamKit2.GC.Dota.Internal"
+..\..\Protogen\protogen -s:..\ -i:"dota_gcmessages_client_match_management.proto" -o:"..\..\..\SteamKit2\SteamKit2\Base\Generated\GC\Dota\MsgGCClientMatchMgmt.cs" -t:csharp -ns:"SteamKit2.GC.Dota.Internal"
+..\..\Protogen\protogen -s:..\ -i:"dota_gcmessages_client_team.proto" -o:"..\..\..\SteamKit2\SteamKit2\Base\Generated\GC\Dota\MsgGCClientTeam.cs" -t:csharp -ns:"SteamKit2.GC.Dota.Internal"
+..\..\Protogen\protogen -s:..\ -i:"dota_gcmessages_client_tournament.proto" -o:"..\..\..\SteamKit2\SteamKit2\Base\Generated\GC\Dota\MsgGCClientTournament.cs" -t:csharp -ns:"SteamKit2.GC.Dota.Internal"
+..\..\Protogen\protogen -s:..\ -i:"dota_gcmessages_client_watch.proto" -o:"..\..\..\SteamKit2\SteamKit2\Base\Generated\GC\Dota\MsgGCClientWatch.cs" -t:csharp -ns:"SteamKit2.GC.Dota.Internal"
 ..\..\Protogen\protogen -s:..\ -i:"dota_gcmessages_server.proto" -o:"..\..\..\SteamKit2\SteamKit2\Base\Generated\GC\Dota\MsgGCServer.cs" -t:csharp -ns:"SteamKit2.GC.Dota.Internal"
