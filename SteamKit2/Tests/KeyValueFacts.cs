9b850ad015580a61da14b31fffe18eec5147c1fd
JustArchi <JustArchi@JustArchi.net>
2016-06-25 15:57:04 +0200

    Fix formatting

--- SteamKit2/Tests/KeyValueFacts.cs
+++ SteamKit2/Tests/KeyValueFacts.cs
@@ -501,27 +501,27 @@ namespace Tests
         }
 
         [Fact]
-		public void KeyValuesUnsignedByteConversion()
+        public void KeyValuesUnsignedByteConversion()
         {
             byte expectedValue = 37;
 
-			var kv = new KeyValue("key", "37");
-			Assert.Equal(kv.AsUnsignedByte(), expectedValue);
+            var kv = new KeyValue("key", "37");
+            Assert.Equal(kv.AsUnsignedByte(), expectedValue);
 
-			kv.Value = "256";
-			Assert.Equal(kv.AsUnsignedByte(expectedValue), expectedValue);
+            kv.Value = "256";
+            Assert.Equal(kv.AsUnsignedByte(expectedValue), expectedValue);
         }
 
-		[Fact]
-		public void KeyValuesUnsignedShortConversion()
+        [Fact]
+        public void KeyValuesUnsignedShortConversion()
         {
             ushort expectedValue = 1337;
 
-			var kv = new KeyValue("key", "1337");
-			Assert.Equal(kv.AsUnsignedShort(), expectedValue);
+            var kv = new KeyValue("key", "1337");
+            Assert.Equal(kv.AsUnsignedShort(), expectedValue);
 
-			kv.Value = "123456";
-			Assert.Equal(kv.AsUnsignedShort(expectedValue), expectedValue);
+            kv.Value = "123456";
+            Assert.Equal(kv.AsUnsignedShort(expectedValue), expectedValue);
         }
 
         const string TestObjectHex = "00546573744F626A65637400016B65790076616C7565000808";
