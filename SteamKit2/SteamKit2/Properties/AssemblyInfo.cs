f111ff0686dd4cc855f9638ae00fb141569e17d4
Netshroud <netshroud@gmail.com>
2017-03-09 17:28:08 +1100

    netstandard: InternalsVisibleTo("Tests")
    not signed when built on macos and linux

--- SteamKit2/SteamKit2/Properties/AssemblyInfo.cs
+++ SteamKit2/SteamKit2/Properties/AssemblyInfo.cs
@@ -35,10 +35,14 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyInformationalVersion( "1.8.1 - Development" )]
 
 #if DEBUG
+#if NETSTANDARD1_3
+[assembly: InternalsVisibleTo( "Tests" )]
+#else
 [assembly: InternalsVisibleTo( "Tests, PublicKey="+
     "00240000048000009400000006020000002400005253413100040000010001009330cecb41df66"+
     "973ee75ded87679584c9ce29bebc7f6c6e1f7accb040a46c0ee0f43afee41b74619d5ef1fad241"+
     "0c50a915537cc54be6ae89b902740feb402e7ae78d5a9570aa8086fafa814773cb7e9e0b010262"+
     "154076ae6261051e4b8537d1165a5e9c35508844d5231b70d71d7a69a6f3056cd611cfeebd4fdb"+
     "a94bf5a6" )]
+    #endif
 #endif
