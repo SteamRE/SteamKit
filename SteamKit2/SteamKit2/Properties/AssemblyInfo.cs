8364e729fef3a086647ea75cae63b6847d8d2f4b
Netshroud <netshroud@gmail.com>
2017-03-13 20:42:40 +1100

    Use `dotnet pack` to produce nuget package

--- SteamKit2/SteamKit2/Properties/AssemblyInfo.cs
+++ SteamKit2/SteamKit2/Properties/AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle( "SteamKit2 ")]
-[assembly: AssemblyDescription( "Steam Networking Library" )]
+[assembly: AssemblyDescription( ".NET library that aims to interoperate with the Steam network." )]
 [assembly: AssemblyConfiguration( "" )]
 [assembly: AssemblyCompany( "SteamRE Team" )]
 [assembly: AssemblyProduct( "SteamKit2" )]
