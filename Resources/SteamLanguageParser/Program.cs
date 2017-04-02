79761a907261d021fd34c3784a26799dc688fe10
Netshroud <netshroud@gmail.com>
2017-04-02 14:41:38 +1000

    Restore old enum values in steamd and associated comments

--- Resources/SteamLanguageParser/Program.cs
+++ Resources/SteamLanguageParser/Program.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
-using System.Text.RegularExpressions;
-using System.IO;
 
 namespace SteamLanguageParser
 {
