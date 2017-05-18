b56fa146ebc6191cef7ad42864abc66cbfa7be25
Netshroud <netshroud@gmail.com>
2017-05-18 19:04:14 +1000

    Revert "Update protobuf generation scripts to use lightFramework"
    This reverts commit bb90a840c2f3ffee53fd1de09f62fdc0184be8ae.
    
    # Conflicts:
    #	Resources/Protobufs/csgo/generate-base.bat
    #	Resources/Protobufs/dota/generate-base.bat
    #	Resources/Protobufs/gc/generate.bat
    #	Resources/Protobufs/steamclient/generate-base.bat
    #	Resources/Protobufs/tf/generate-base.bat

--- Resources/Protobufs/gc/generate.bat
+++ Resources/Protobufs/gc/generate.bat
@@ -1,3 +1,3 @@
 @echo off
 
-..\..\Protogen\protogen -s:..\ -i:"gc.proto" -o:"..\..\..\SteamKit2\SteamKit2\Base\Generated\GC\MsgBaseGC.cs" -t:csharp -p:lightFramework=true -ns:"SteamKit2.GC.Internal" -p:detectMissing
+..\..\Protogen\protogen -s:..\ -i:"gc.proto" -o:"..\..\..\SteamKit2\SteamKit2\Base\Generated\GC\MsgBaseGC.cs" -t:csharp -ns:"SteamKit2.GC.Internal" -p:detectMissing
