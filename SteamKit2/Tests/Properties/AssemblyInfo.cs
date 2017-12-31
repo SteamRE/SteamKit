cf8b1eae05526087382f8f7d1b9911643a23659f
Yaakov <git@yaakov.online>
2018-01-01 00:39:17 +1100

    Happy New Year!

--- SteamKit2/Tests/Properties/AssemblyInfo.cs
+++ SteamKit2/Tests/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using Xunit;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("SteamRE Team")]
 [assembly: AssemblyProduct("SteamKit2Tests")]
-[assembly: AssemblyCopyright("Copyright © SteamRE Team 2017")]
+[assembly: AssemblyCopyright("Copyright © SteamRE Team 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
