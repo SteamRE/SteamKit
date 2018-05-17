e0abf58549485cf7a32d38b2faeabbad6f429f00
Yaakov <git@yaakov.online>
2018-05-17 22:30:35 +1000

    Add a simple Console debug listener.
    Resolves #219.

---
+++ SteamKit2/SteamKit2/Util/SimpleConsoleDebugListener.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SteamKit2
+{
+    /// <summary>
+    /// A debug listener that writes debug output to the system console.
+    /// </summary>
+    public sealed class SimpleConsoleDebugListener : IDebugListener
+    {
+        /// <summary>
+        /// Called when the DebugLog wishes to inform listeners of debug spew.
+        /// </summary>
+        /// <param name="category">The category of the message.</param>
+        /// <param name="msg">The message to log.</param>
+        public void WriteLine(string category, string msg)
+        {
+            Console.WriteLine("[{0}]: {1}", category, msg);
+        }
+    }
+}
