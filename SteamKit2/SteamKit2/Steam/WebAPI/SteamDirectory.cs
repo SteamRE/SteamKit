b0b3e24736ee5e0abbe6143087023cab79c25971
Yaakov <git@yaakov.online>
2018-07-21 21:05:55 +1000

    Make SteamDirectory async instead of using continuations

--- SteamKit2/SteamKit2/Steam/WebAPI/SteamDirectory.cs
+++ SteamKit2/SteamKit2/Steam/WebAPI/SteamDirectory.cs
@@ -40,7 +40,7 @@ namespace SteamKit2
         public static Task<IReadOnlyCollection<ServerRecord>> LoadAsync( SteamConfiguration configuration, int maxNumServers, CancellationToken cancellationToken )
             => LoadCoreAsync( configuration, maxNumServers, cancellationToken );
 
-        static Task<IReadOnlyCollection<ServerRecord>> LoadCoreAsync( SteamConfiguration configuration, int? maxNumServers, CancellationToken cancellationToken )
+        static async Task<IReadOnlyCollection<ServerRecord>> LoadCoreAsync( SteamConfiguration configuration, int? maxNumServers, CancellationToken cancellationToken )
         {
             if ( configuration == null )
             {
@@ -60,40 +60,37 @@ namespace SteamKit2
 
             cancellationToken.ThrowIfCancellationRequested();
 
-            var task = directory.CallAsync( HttpMethod.Get, "GetCMList", version: 1, args: args );
-            return task.ContinueWith( t =>
+            var response = await directory.CallAsync( HttpMethod.Get, "GetCMList", version: 1, args: args ).ConfigureAwait( false );
+
+            var result = ( EResult )response[ "result" ].AsInteger( ( int )EResult.Invalid );
+            if ( result != EResult.OK )
             {
-                var response = task.Result;
-                var result = ( EResult )response[ "result" ].AsInteger( ( int )EResult.Invalid );
-                if ( result != EResult.OK )
+                throw new InvalidOperationException( string.Format( "Steam Web API returned EResult.{0}", result ) );
+            }
+
+            var socketList = response[ "serverlist" ];
+            var websocketList = response[ "serverlist_websockets" ];
+
+            cancellationToken.ThrowIfCancellationRequested();
+
+            var serverRecords = new List<ServerRecord>( capacity: socketList.Children.Count + websocketList.Children.Count );
+
+            foreach ( var child in socketList.Children )
+            {
+                if ( !NetHelpers.TryParseIPEndPoint( child.Value, out var endpoint ) )
                 {
-                    throw new InvalidOperationException( string.Format( "Steam Web API returned EResult.{0}", result ) );
+                    continue;
                 }
 
-                var socketList = response[ "serverlist" ];
-                var websocketList = response[ "serverlist_websockets" ];
+                serverRecords.Add( ServerRecord.CreateSocketServer( endpoint ) );
+            }
 
-                cancellationToken.ThrowIfCancellationRequested();
+            foreach ( var child in websocketList.Children )
+            {
+                serverRecords.Add( ServerRecord.CreateWebSocketServer( child.Value ) );
+            }
 
-                var serverRecords = new List<ServerRecord>( capacity: socketList.Children.Count + websocketList.Children.Count );
-
-                foreach ( var child in socketList.Children )
-                {
-                    if ( !NetHelpers.TryParseIPEndPoint( child.Value, out var endpoint ) )
-                    {
-                        continue;
-                    }
-
-                    serverRecords.Add( ServerRecord.CreateSocketServer( endpoint ) );
-                }
-
-                foreach ( var child in websocketList.Children )
-                {
-                    serverRecords.Add( ServerRecord.CreateWebSocketServer( child.Value ) );
-                }
-
-                return (IReadOnlyCollection<ServerRecord>)serverRecords;
-            }, cancellationToken, TaskContinuationOptions.NotOnCanceled | TaskContinuationOptions.NotOnFaulted, TaskScheduler.Current );
+            return serverRecords;
         }
     }
 }
