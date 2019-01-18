c79f2ec6a743836f22e7305635030c8af006ceae
Pavel Djundik <xPaw@users.noreply.github.com>
2019-01-18 21:18:28 +0200

    Update to latest protogen

--- Resources/Protogen/generate-all.ps1
+++ Resources/Protogen/generate-all.ps1
@@ -11,49 +11,48 @@
 #>
 param([string[]]$ProtoDir)
 
-$ProtoGenDir = $PSScriptRoot
 $ProtoGenExe = Join-Path $PSScriptRoot 'protogen.exe'
-$ProtoBase = Join-Path $PSScriptRoot '..\ProtoBufs'
-$SK2Base = Join-Path $PSScriptRoot '..\..\SteamKit2\SteamKit2\Base\Generated'
-
-Push-Location
+$ProtoProcessedBase = Join-Path $PSScriptRoot 'ProcessedProtoBufs'
+$ProtoBase = Join-Path $PSScriptRoot '..\ProtoBufs' -Resolve
+$SK2Base = Join-Path $PSScriptRoot '..\..\SteamKit2\SteamKit2\Base\Generated' -Resolve
 
 $protos = Import-Csv -LiteralPath (Join-Path $PSScriptRoot 'protos.csv') |
-    ? { (!$ProtoDir) -or ($_.ProtoDir -in $ProtoDir)}
+    Where-Object { (!$ProtoDir) -or ($_.ProtoDir -in $ProtoDir)}
 
-$CommonParams = @(
-    '-s:"{0}"' -f $ProtoBase,
-    '-t:csharp'
-    '-p:detectMissing'
-)
+if ( Test-Path $ProtoProcessedBase )
+{
+    Remove-Item -Recurse -Force $ProtoProcessedBase
+}
 
 # one-off
-Set-Location -LiteralPath $PSScriptRoot
-$params = $CommonParams + @(
-    '-i:"gc.proto"'
-    '-o:"{0}"' -f (Join-Path $SK2Base 'GC\MsgBaseGC.cs')
-    '-ns:"SteamKit2.GC.Internal"'
+$params = @(
+    '--proto_path="{0}"' -f $PSScriptRoot
+    '--csharp_out="{0}"' -f (Join-Path $ProtoProcessedBase 'GC')
+    '+langver=7.0'
+    '"gc.proto"'
 )
 
 & $ProtoGenExe $params > $null
 
-$protos | % {
-    Set-Location -LiteralPath (Join-Path $ProtoBase $_.ProtoDir)
-    $params = $CommonParams + @(
-        '-i:"{0}"' -f $_.ProtoFileName
-        '-o:"{0}"' -f (Join-Path $SK2Base $_.ClassFilePath)
-        '-ns:"{0}"' -f $_.Namespace
+$protos | ForEach-Object {
+    $InputProtoDir = Join-Path $ProtoBase $_.ProtoDir
+    $OutputProtoFile = Join-Path $ProtoProcessedBase $_.ClassFilePath
+    $OutputProtoDir = Split-Path -Path $OutputProtoFile
+    $OutputProtoCompiled = Join-Path $OutputProtoDir $($_.ProtoFileName.Substring(0, $_.ProtoFileName.Length - 6) + ".cs")
+
+    $params = @(
+        '--proto_path="{0}"' -f $InputProtoDir
+        '--csharp_out="{0}"' -f $OutputProtoDir
+        '--package="{0}"' -f $_.Namespace
+        '+langver=7.0'
+        $_.ProtoFileName
     )
 
-    & $ProtoGenExe $params > $null
+    & $ProtoGenExe $params
 
-    # Hack to work around protogen stderr messges not ending the line
-    if ($LASTEXITCODE -ne 0)
-    {
-        Write-Host
-    }
+    Copy-Item $OutputProtoCompiled -Destination (Join-Path $SK2Base $_.ClassFilePath)
 
-    $_
+    Write-Output ""
 }
 
-Pop-Location
+Remove-Item -Recurse -Force $ProtoProcessedBase
