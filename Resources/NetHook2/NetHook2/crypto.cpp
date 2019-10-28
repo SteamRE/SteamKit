18a9cf23d0b9a1f25f7a56dfb0e6ac817b40bfa1
m4dEngi <m4dengi@gmail.com>
2019-10-28 11:33:09 +0100

    Using steamclients own PchMsgNameFromEMsg() for EMsg names lookup

--- Resources/NetHook2/NetHook2/crypto.cpp
+++ Resources/NetHook2/NetHook2/crypto.cpp
@@ -11,18 +11,16 @@
 
 
 SymmetricEncryptChosenIVFn Encrypt_Orig = nullptr;
-bool (__cdecl *GetMessageFn)( int * ) = nullptr;
-
+PchMsgNameFromEMsgFn PchMsgNameFromEMsg = nullptr;
 
 static_assert(sizeof(MsgInfo_t) == 20, "Wrong size of MsgInfo_t");
 static_assert(offsetof(MsgInfo_t, eMsg) == 0, "Wrong offset of MsgInfo_t::eMsg");
-static_assert(offsetof(MsgInfo_t, pchMsgName) == 4, "Wrong offset of MsgInfo_t::pchMsgName");
-static_assert(offsetof(MsgInfo_t, nFlags) == 8, "Wrong offset of MsgInfo_t::nFlags");
-static_assert(offsetof(MsgInfo_t, k_EServerTarget) == 12, "Wrong offset of MsgInfo_t::k_EServerTarget");
-static_assert(offsetof(MsgInfo_t, nUnk1) == 16, "Wrong offset of MsgInfo_t::uUnk1");
+static_assert(offsetof(MsgInfo_t, nFlags) == 4, "Wrong offset of MsgInfo_t::nFlags");
+static_assert(offsetof(MsgInfo_t, k_EServerTarget) == 8, "Wrong offset of MsgInfo_t::k_EServerTarget");
+static_assert(offsetof(MsgInfo_t, nUnk1) == 12, "Wrong offset of MsgInfo_t::uUnk1");
+static_assert(offsetof(MsgInfo_t, pchMsgName) == 16, "Wrong offset of MsgInfo_t::pchMsgName");
 
 typedef std::pair<EMsg, MsgInfo_t *> MsgPair;
-
 
 CCrypto::CCrypto() noexcept
 	: Encrypt_Detour( nullptr )
@@ -41,74 +39,19 @@ CCrypto::CCrypto() noexcept
 
 	g_pLogger->LogConsole( "CCrypto::SymmetricEncryptChosenIV = 0x%x\n", Encrypt_Orig );
 
-	char *pGetMessageList = nullptr;
-	const bool bGetMessageList = steamClientScan.FindFunction(
-		"\xA1\x00\x00\x00\x00\xA8\x01\x75\x29\x68\x00\x00\x00\x00\x83\xC8\x01\xB9\x00\x00\x00\x00\x68\x00\x00\x00\x00\xA3\x00\x00\x00\x00\xE8\x00\x00\x00\x00\x68\x00\x00\x00\x00\xE8\x00\x00\x00\x00\x83\xC4\x04\xB8\x00\x00\x00\x00\xC3",
-		"x????xxxxx????xxxx????x????x????x????x????x????xxxx????x",
-		(void **)&pGetMessageList
+	const bool bPchMsgNameFromEMsg = steamClientScan.FindFunction(
+		"\x55\x8B\xEC\xA1\x00\x00\x00\x00\x83\xEC\x08\xA8\x01\x75\x1F\x83\xC8\x01\xB9\x00\x00\x00\x00\xA3",
+		"xxxx????xxxxxxxxxxx????x",
+		(void **)&PchMsgNameFromEMsg
 	);
 
-	if (bGetMessageList)
+	if (bPchMsgNameFromEMsg)
 	{
-		constexpr uint32 uMessageListStartPtrOffset = 23;
-		constexpr uint32 uMessageListCountPtrOffset = 10;
-
-		MsgInfo_t *pInfos = *(MsgInfo_t **)( pGetMessageList + uMessageListStartPtrOffset );
-		const uint32 uNumMessages = *(uint32 *)( pGetMessageList + uMessageListCountPtrOffset );
-
-		g_pLogger->LogConsole( "pGetMessageList = 0x%x\npInfos = 0x%x\nnumMessages = %d\n", pGetMessageList, pInfos, uNumMessages );
-		
-		HMODULE sc = GetModuleHandle("steamclient.dll");
-		assert(sc != nullptr);
-		MODULEINFO modInfo;
-		GetModuleInformation(GetCurrentProcess(), sc, &modInfo, sizeof(modInfo));
-
-		const uintptr_t uSteamAddrMin = (uintptr_t)modInfo.lpBaseOfDll;
-		const uintptr_t uSteamAddrMax = (uintptr_t)modInfo.lpBaseOfDll + modInfo.SizeOfImage;
-
-		for ( uint16 x = 0 ; x < uNumMessages; x++ )
-		{
-			const uintptr_t uMsgNameAddr = (uintptr_t)pInfos->pchMsgName;
-			if (uMsgNameAddr < uSteamAddrMin || uMsgNameAddr >= uSteamAddrMax)
-			{
-				g_pLogger->LogConsole("Found bad emsg name. Check size and layout of MsgInfo_t. Aborting emsg lookup.\n");
-				break;
-			}
-			eMsgList.insert( MsgPair( pInfos->eMsg, pInfos ) );
-
-			pInfos++;
-		}
-
-		if ( eMsgList.size() != 0 )
-		{
-			// should only delete our existing files if we have something new to dump
-			g_pLogger->DeleteFile( "emsg_list.txt", false );
-			g_pLogger->DeleteFile( "emsg_list_detailed.txt", false );
-
-			HANDLE hListFile = g_pLogger->OpenFile( "emsg_list.txt", false );
-			HANDLE hListDetailedFile = g_pLogger->OpenFile( "emsg_list_detailed.txt", false );
-
-			for ( MsgList::iterator iter = eMsgList.begin() ; iter != eMsgList.end() ; iter++ )
-			{
-				const MsgInfo_t * pInfo = iter->second;
-
-				g_pLogger->LogOpenFile( hListFile, "\t%s = %d,\r\n", pInfo->pchMsgName, pInfo->eMsg );
-				g_pLogger->LogOpenFile( hListDetailedFile, "\t%s = %d, // flags: %d, server type: %d\r\n", pInfo->pchMsgName, pInfo->eMsg, pInfo->nFlags, pInfo->k_EServerTarget );
-			}
-
-			g_pLogger->CloseFile( hListFile );
-			g_pLogger->CloseFile( hListDetailedFile );
-
-			g_pLogger->LogConsole( "Dumped emsg list! (%d messages)\n", eMsgList.size() );
-		}
-		else
-		{
-			g_pLogger->LogConsole( "Unable to dump emsg list: No messages! (Offset changed?)\n" );
-		} 
+		g_pLogger->LogConsole( "PchMsgNameFromEMsg = 0x%x\n", PchMsgNameFromEMsg);
 	}
 	else
 	{
-		g_pLogger->LogConsole( "Unable to find GetMessageList.\n" );
+		g_pLogger->LogConsole( "Unable to find PchMsgNameFromEMsg.\n" );
 	}
 
 	SymmetricEncryptChosenIVFn encrypt = CCrypto::SymmetricEncryptChosenIV;
@@ -128,8 +71,6 @@ CCrypto::CCrypto() noexcept
 
 CCrypto::~CCrypto()
 {
-	eMsgList.clear();
-
 	if ( Encrypt_Detour )
 	{
 		Encrypt_Detour->Detach();
@@ -150,12 +91,9 @@ bool __cdecl CCrypto::SymmetricEncryptChosenIV( const uint8 *pubPlaintextData, u
 
 const char* CCrypto::GetMessage( EMsg eMsg, uint8 serverType )
 {
-	for ( MsgList::iterator iter = eMsgList.begin() ; iter != eMsgList.end() ; iter++ )
+	if(PchMsgNameFromEMsg != nullptr)
 	{
-		if ( iter->first == eMsg )
-		{
-			return iter->second->pchMsgName;
-		}
+		return PchMsgNameFromEMsg(eMsg);
 	}
 
 	return nullptr;
