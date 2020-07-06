b8453f3502d879f65b5bafeaf51f6ff2e96c5603
Yaakov <git@yaakov.online>
2020-07-06 20:39:28 +1000

    Update NHA2 for protobuf-net 3.0

--- Resources/NetHookAnalyzer2/NetHookAnalyzer2/ProtoBufFieldReader.cs
+++ Resources/NetHookAnalyzer2/NetHookAnalyzer2/ProtoBufFieldReader.cs
@@ -10,71 +10,69 @@ namespace NetHookAnalyzer2
 		public static Dictionary<int, List<object>> ReadProtobuf(Stream stream)
 		{
 			// try reading it as a protobuf
-			using (var reader = ProtoReader.Create(stream, null))
+			var reader = ProtoReader.State.Create( stream, null );
+			var fields = new Dictionary<int, List<object>>();
+
+			while (true)
 			{
-				var fields = new Dictionary<int, List<object>>();
+				int field = reader.ReadFieldHeader();
 
-				while (true)
+				if (field == 0)
+					break;
+
+				object fieldValue = null;
+
+				switch (reader.WireType)
 				{
-					int field = reader.ReadFieldHeader();
-
-					if (field == 0)
-						break;
-
-					object fieldValue = null;
-
-					switch (reader.WireType)
-					{
-						case WireType.Variant:
-						case WireType.Fixed32:
-						case WireType.Fixed64:
-						case WireType.SignedVariant:
+					case WireType.Varint:
+					case WireType.Fixed32:
+					case WireType.Fixed64:
+					case WireType.SignedVarint:
+						{
+							try
 							{
-								try
-								{
-									fieldValue = reader.ReadInt64();
-								}
-								catch (Exception)
-								{
-									fieldValue = "Unable to read Variant (debugme)";
-								}
-
-								break;
+								fieldValue = reader.ReadInt64();
 							}
-						case WireType.String:
+							catch (Exception)
 							{
-								try
-								{
-									fieldValue = reader.ReadString();
-								}
-								catch (Exception)
-								{
-									fieldValue = "Unable to read String (debugme)";
-								}
-
-								break;
+								fieldValue = "Unable to read Variant (debugme)";
 							}
-						default:
+
+							break;
+						}
+					case WireType.String:
+						{
+							try
 							{
-								fieldValue = string.Format("{0} is not implemented", reader.WireType);
-								break;
+								fieldValue = reader.ReadString();
 							}
-					}
+							catch (Exception)
+							{
+								fieldValue = "Unable to read String (debugme)";
+							}
 
-					if (fields.TryGetValue(field, out var values))
-					{
-						values.Add( fieldValue );
-					}
-					else
-					{
-						values = new List<object> { fieldValue };
-						fields[ field ] = values;
-					}
+							break;
+						}
+					default:
+						{
+							fieldValue = string.Format("{0} is not implemented", reader.WireType);
+							break;
+						}
 				}
 
-				if (fields.Count > 0)
-					return fields;
+				if (fields.TryGetValue(field, out var values))
+				{
+					values.Add( fieldValue );
+				}
+				else
+				{
+					values = new List<object> { fieldValue };
+					fields[ field ] = values;
+				}
 			}
+
+			if (fields.Count > 0)
+				return fields;
 
 			return null;
 		}
