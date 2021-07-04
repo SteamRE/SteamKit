e95cb8bc5112ddcb30792914c6508e6da940b231
Yaakov <git@yaakov.dev>
2021-07-04 18:20:30 +1000

    Copy trimming attributes for pre-net5 builds

---
+++ SteamKit2/SteamKit2/Util/TrimmingAttributes.cs
@@ -0,0 +1,91 @@
+﻿namespace System.Diagnostics.CodeAnalysis
+{
+    static class Trimming
+    {
+        public const DynamicallyAccessedMemberTypes ForProtobufNet =
+            DynamicallyAccessedMemberTypes.PublicConstructors |
+            DynamicallyAccessedMemberTypes.NonPublicConstructors |
+            DynamicallyAccessedMemberTypes.PublicMethods |
+            DynamicallyAccessedMemberTypes.NonPublicMethods |
+            DynamicallyAccessedMemberTypes.PublicFields |
+            DynamicallyAccessedMemberTypes.NonPublicFields |
+            DynamicallyAccessedMemberTypes.PublicProperties |
+            DynamicallyAccessedMemberTypes.NonPublicProperties;
+    }
+
+#if !NET5_0_OR_GREATER
+
+    [Flags]
+    internal enum DynamicallyAccessedMemberTypes
+    {
+        All = -1,
+        None = 0,
+        PublicParameterlessConstructor = 1,
+        PublicConstructors = 3,
+        NonPublicConstructors = 4,
+        PublicMethods = 8,
+        NonPublicMethods = 16,
+        PublicFields = 32,
+        NonPublicFields = 64,
+        PublicNestedTypes = 128,
+        NonPublicNestedTypes = 256,
+        PublicProperties = 512,
+        NonPublicProperties = 1024,
+        PublicEvents = 2048,
+        NonPublicEvents = 4096,
+        Interfaces = 8192
+    }
+
+    [AttributeUsage( AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Method | AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Interface | AttributeTargets.Parameter | AttributeTargets.ReturnValue | AttributeTargets.GenericParameter, Inherited = false )]
+    internal sealed class DynamicallyAccessedMembersAttribute : Attribute
+    {
+        public DynamicallyAccessedMembersAttribute( DynamicallyAccessedMemberTypes memberTypes )
+        {
+        }
+
+        public DynamicallyAccessedMemberTypes MemberTypes { get; }
+    }
+
+    [AttributeUsage(AttributeTargets.Constructor | AttributeTargets.Method | AttributeTargets.Field, AllowMultiple = true, Inherited = false)]
+    internal sealed class DynamicDependencyAttribute : Attribute
+    {
+        public DynamicDependencyAttribute(string memberSignature )
+        {
+            MemberSignature = memberSignature;
+        }
+
+        public DynamicDependencyAttribute( DynamicallyAccessedMemberTypes memberTypes, Type type )
+        {
+            MemberTypes = memberTypes;
+            Type = type;
+        }
+
+        public DynamicDependencyAttribute(string memberSignature, Type type)
+        {
+            MemberSignature = memberSignature;
+            Type = type;
+        }
+
+        public DynamicDependencyAttribute(DynamicallyAccessedMemberTypes memberTypes, string typeName, string assemblyName)
+        {
+            MemberTypes = memberTypes;
+            TypeName = typeName;
+            AssemblyName = assemblyName;
+        }
+
+        public DynamicDependencyAttribute(string memberSignature, string typeName, string assemblyName)
+        {
+            MemberSignature = memberSignature;
+            TypeName = typeName;
+            AssemblyName = assemblyName;
+        }
+
+        public string? AssemblyName { get; }
+        public string? Condition { get; set; }
+        public string? MemberSignature { get; }
+        public DynamicallyAccessedMemberTypes MemberTypes { get; }
+        public Type? Type { get; }
+        public string? TypeName { get; }
+    }
+#endif
+}
