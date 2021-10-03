61c80bfb8f60e8fe68358edb5b61c060d797312e
Yaakov <git@yaakov.dev>
2021-10-03 14:33:47 +1100

    Add more machine info tests - background processing, garbage collection

--- SteamKit2/Tests/MachineInfoFacts.cs
+++ SteamKit2/Tests/MachineInfoFacts.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading;
 using SteamKit2;
 using Xunit;
 
@@ -49,6 +50,32 @@ namespace Tests
             Assert.Equal(invocations, provider.TotalInvocations);
         }
 
+        [Fact]
+        public void MachineInfoIsProcessedInBackground()
+        {
+            var provider = new ThreadRejectingMachineInfoProvider(Thread.CurrentThread.ManagedThreadId);
+            HardwareUtils.Init(provider);
+
+            // Should not throw
+            HardwareUtils.GetMachineID(provider);
+        }
+
+        [Fact]
+        public void ProviderIsNotRetained()
+        {
+            WeakReference Setup()
+            {
+                var provider = new CountingMachineInfoProvider();
+                HardwareUtils.Init(provider);
+                HardwareUtils.GetMachineID(provider);
+                return new WeakReference(provider);
+            }
+
+            var provider = Setup();
+            GC.Collect();
+            Assert.False(provider.IsAlive);
+        }
+
         sealed class CountingMachineInfoProvider : IMachineInfoProvider
         {
             public int TotalInvocations { get; private set; }
@@ -68,6 +95,42 @@ namespace Tests
             {
                 TotalInvocations++;
                 return Array.Empty<byte>();
+            }
+        }
+
+        sealed class ThreadRejectingMachineInfoProvider : IMachineInfoProvider
+        {
+            public ThreadRejectingMachineInfoProvider(int threadId)
+            {
+                ThreadIdToReject = threadId;
+            }
+
+            public int ThreadIdToReject { get; }
+
+            public byte[] GetDiskId()
+            {
+                EnsureNotOnRejectedThread();
+                return Array.Empty<byte>();
+            }
+
+            public byte[] GetMacAddress()
+            {
+                EnsureNotOnRejectedThread();
+                return Array.Empty<byte>();
+            }
+
+            public byte[] GetMachineGuid()
+            {
+                EnsureNotOnRejectedThread();
+                return Array.Empty<byte>();
+            }
+
+            void EnsureNotOnRejectedThread()
+            {
+                if (Thread.CurrentThread.ManagedThreadId == ThreadIdToReject)
+                {
+                    throw new InvalidOperationException("Operation must not be run on rejected thread.");
+                }
             }
         }
     }
