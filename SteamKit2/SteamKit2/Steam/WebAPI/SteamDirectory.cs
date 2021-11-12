14dc5af7306103f504756e0bc4efcff62e7d50a6
Yaakov <git@yaakov.dev>
2021-11-12 15:35:44 +1100

    Fix last nullability warnings

--- SteamKit2/SteamKit2/Steam/WebAPI/SteamDirectory.cs
+++ SteamKit2/SteamKit2/Steam/WebAPI/SteamDirectory.cs
@@ -48,7 +48,7 @@ namespace SteamKit2
             }
 
             var directory = configuration.GetAsyncWebAPIInterface( "ISteamDirectory" );
-            var args = new Dictionary<string, object>
+            var args = new Dictionary<string, object?>
             {
                 ["cellid"] = configuration.CellID.ToString( CultureInfo.InvariantCulture )
             };
