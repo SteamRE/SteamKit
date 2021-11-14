ae2b29064cbcb0978f53d5a3fb7b4a43ebb56f96
Pavel Djundik <xPaw@users.noreply.github.com>
2021-11-14 23:21:37 +0200

    Use UriBuilder for websocket uris; fix ipv6

---
+++ SteamKit2/Tests/WebSocketUriFacts.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using SteamKit2;
+using Xunit;
+
+namespace Tests
+{
+    public class WebSocketUriFacts
+    {
+        [Fact]
+        public void DnsEndPoint()
+        {
+            var endpoint = new DnsEndPoint( "example.com", 1337 );
+            Assert.Equal( "wss://example.com:1337/cmsocket/", WebSocketConnection.WebSocketContext.ConstructUri( endpoint ).ToString() );
+        }
+
+        [Fact]
+        public void IPEndPointV4()
+        {
+            var endpoint = new IPEndPoint( IPAddress.Loopback, 1337 );
+            Assert.Equal( "wss://127.0.0.1:1337/cmsocket/", WebSocketConnection.WebSocketContext.ConstructUri( endpoint ).ToString() );
+        }
+
+        [Fact]
+        public void IPEndPointV6()
+        {
+            var endpoint = new IPEndPoint( IPAddress.IPv6Loopback, 1337 );
+            Assert.Equal( "wss://[::1]:1337/cmsocket/", WebSocketConnection.WebSocketContext.ConstructUri( endpoint ).ToString() );
+        }
+
+        [Fact]
+        public void ThrowsWrongEndPoint()
+        {
+            Assert.Throws<InvalidOperationException>( () => WebSocketConnection.WebSocketContext.ConstructUri( new DummyEndPoint() ) );
+        }
+
+        class DummyEndPoint : EndPoint
+        {
+        }
+    }
+}
