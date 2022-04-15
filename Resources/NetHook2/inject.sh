2bcf87ebdd88fe7564819c53fa1d49f0e62888b7
m4dengi <m4dengi@gmail.com>
2022-04-15 22:07:55 +0300

    Uglyfied linux injector script to accept target PID as argument and use pidof instead of ps

--- Resources/NetHook2/inject.sh
+++ Resources/NetHook2/inject.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-STEAM_PID=$(ps -C steam -o pid= | head -1)
+STEAM_PID=$(pidof steam | xargs -n1 | sort -g | head -1)
 LIB_PL="${PWD}/libtuxhookldr.so"
 INJECTOR="${PWD}/tuxjector"
 
-if [ -n "${STEAM_PID}" ]
-    then
+if [ -n "${1}" ]
+	then
+		${INJECTOR} ${LIB_PL} ${1}
+elif [ -n "${STEAM_PID}" ]
+	then
 		${INJECTOR} ${LIB_PL} ${STEAM_PID}
-    else
-        echo "Steam PID not found!"
+else
+	echo "Steam PID not found!"
 fi
