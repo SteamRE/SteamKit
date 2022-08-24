2ee483dc2e619145fd6c2f829c076eac280a4489
Nick Hastings <nshastings@gmail.com>
2022-08-24 17:27:44 -0400

    NHA: Update Dota 2 SO Helper

--- Resources/NetHookAnalyzer2/NetHookAnalyzer2/Specializations/Dota2/Dota2SOHelper.cs
+++ Resources/NetHookAnalyzer2/NetHookAnalyzer2/Specializations/Dota2/Dota2SOHelper.cs
@@ -13,15 +13,12 @@ namespace NetHookAnalyzer2.Specializations
         public static Dictionary<int, Type> SOTypes = new Dictionary<int, Type>()
         {
             {1, typeof(CSOEconItem)},
-            {5, typeof(CSOItemRecipe)},
             {7, typeof(CSOEconGameAccountClient)},
             {38, typeof(CSOEconItemDropRateBonus)},
-            {39, typeof(CSOEconItemLeagueViewPass)},
-            {40, typeof(CSOEconItemEventTicket)},
             {42, typeof(CSOEconItemTournamentPassport)},
             {2002, typeof(CSODOTAGameAccountClient)},
             {2003, typeof(CSODOTAParty)},
-            {2004, typeof(CSODOTALobby)},
+            {2004, typeof(CSODOTALobby)}, // CSODOTADynamicLobby
             {2006, typeof(CSODOTAPartyInvite)},
             {2007, typeof(CSODOTAGameHeroFavorites)},
             {2008, typeof(CSODOTAMapLocationState)},
@@ -29,6 +26,10 @@ namespace NetHookAnalyzer2.Specializations
             {2010, typeof(CSODOTAPlayerChallenge)},
             {2011, typeof(CSODOTALobbyInvite)},
             {2012, typeof(CSODOTAGameAccountPlus)},
+            {2013, typeof(CSODOTALobby)},
+            {2014, typeof(CSODOTAStaticLobby)},
+            {2015, typeof(CSODOTAServerLobby)},
+            {2016, typeof(CSODOTAServerStaticLobby)},
         };
     }
 }
