11e843e652d9c0190b74761c839db160556d1d23
Pavel Djundik <xPaw@users.noreply.github.com>
2022-10-30 20:08:00 +0200

    Fix list view flickering

---
+++ Resources/NetHookAnalyzer2/NetHookAnalyzer2/ListViewDoubleBuffered.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace NetHookAnalyzer2
+{
+	[ToolboxItem( true )]
+	[ToolboxBitmap( typeof( ListView ) )]
+	class ListViewDoubleBuffered : ListView
+	{
+		public ListViewDoubleBuffered()
+		{
+			this.DoubleBuffered = true;
+		}
+	}
+}
