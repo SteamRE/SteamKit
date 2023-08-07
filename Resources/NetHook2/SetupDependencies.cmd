7abb2fbc3ffbcdf4bf02526ce159e3d85d6fb0b0
Nick Hastings <nshastings@gmail.com>
2023-08-07 12:36:33 -0400

    Numerous fixes for deps script

--- Resources/NetHook2/SetupDependencies.cmd
+++ Resources/NetHook2/SetupDependencies.cmd
@@ -1,27 +1,29 @@
 @echo off
 
-for /f "usebackq tokens=*" %%i in (`"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath`) do (
-  set InstallDir=%%i
+if not defined DevEnvDir (
+    for /f "usebackq tokens=*" %%i in (`"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath`) do (
+        set InstallDir=%%i
+    )
+
+    if exist "%InstallDir%\Common7\Tools\vsdevcmd.bat" (
+        call "%InstallDir%\Common7\Tools\vsdevcmd.bat"
+    )
 )
 
-if exist "%InstallDir%\Common7\Tools\vsdevcmd.bat" (
-  call "%InstallDir%\Common7\Tools\vsdevcmd.bat"
-)
-
-where /q vcpkg
-if ERRORLEVEL 1 (
-    if not exist vcpkg/ (
-        git clone https://github.com/Microsoft/vcpkg.git
+where.exe /q vcpkg
+if %ERRORLEVEL%==1 (
+    if not exist "vcpkg/" (
+        git clone --depth 1 "https://github.com/Microsoft/vcpkg.git"
     ) else (
         cd vcpkg
         git pull
         cd ..
     )
 
-    .\vcpkg\bootstrap-vcpkg.bat
-    SET PATH=%PATH%;%CD%\vcpkg
+    call ".\vcpkg\bootstrap-vcpkg.bat"
+    SET "PATH=%CD%\vcpkg;%PATH%"
 )
 
-vcpkg integrate project
+vcpkg integrate install
 
 rem todo: compile or download protoc and generate steammessages_base.pb.{h|cpp}
