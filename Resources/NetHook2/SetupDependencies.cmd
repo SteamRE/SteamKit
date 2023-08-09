da06c726f2808c7ae14d76af6c2c95674609839d
Nick Hastings <nshastings@gmail.com>
2023-08-09 08:01:02 -0400

    Add debug output to SetupDependencies

--- Resources/NetHook2/SetupDependencies.cmd
+++ Resources/NetHook2/SetupDependencies.cmd
@@ -12,17 +12,25 @@ if not defined DevEnvDir (
 
 where.exe /q vcpkg
 if %ERRORLEVEL%==1 (
+    echo "vcpkg not found in path. Checking to see if we're running under CI..."
+
     if defined CI (
+        echo "CI environment detected."
         if not exist "vcpkg/" (
+            echo "Cloning vcpkg from GitHub..."
             git clone --depth 1 "https://github.com/Microsoft/vcpkg.git"
         ) else (
+            echo "Updating existing vcpkg clone..."
             cd vcpkg
             git pull
             cd ..
         )
+        echo "Bootstrapping vcpkg..."
         call ".\vcpkg\bootstrap-vcpkg.bat"
         SET "PATH=%CD%\vcpkg;%PATH%"
+        echo "Installing vcpkg..."
         vcpkg integrate install
+        echo "Finished setting up vcpkg."
     ) else (
         echo "vcpkg is required but not found. Please see https://vcpkg.io/en/getting-started to install it
     )    
