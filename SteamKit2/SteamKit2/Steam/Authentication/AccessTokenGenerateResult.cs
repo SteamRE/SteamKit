2282c5dce00d088f0d1051ea8a6da20de574f541
Pavel Djundik <xPaw@users.noreply.github.com>
2023-10-18 13:00:45 +0300

    Add `GenerateAccessTokenForAppAsync`

---
+++ SteamKit2/SteamKit2/Steam/Authentication/AccessTokenGenerateResult.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * This file is subject to the terms and conditions defined in
+ * file 'license.txt', which is part of this source code package.
+ */
+
+using SteamKit2.Internal;
+
+namespace SteamKit2.Authentication
+{
+    /// <summary>
+    /// Represents access token generation result.
+    /// </summary>
+    public sealed class AccessTokenGenerateResult
+    {
+        /// <summary>
+        /// New refresh token.
+        /// This can be provided to <see cref="SteamUser.LogOnDetails.AccessToken"/>.
+        /// </summary>
+        public string RefreshToken { get; }
+        /// <summary>
+        /// New token subordinate to <see cref="RefreshToken"/>.
+        /// </summary>
+        public string AccessToken { get; }
+
+        internal AccessTokenGenerateResult( CAuthentication_AccessToken_GenerateForApp_Response response )
+        {
+            AccessToken = response.access_token;
+            RefreshToken = response.refresh_token;
+        }
+    }
+}
