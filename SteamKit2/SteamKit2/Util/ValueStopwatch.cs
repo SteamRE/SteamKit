e2b1c3238b32fafe7b6743f9ce34481c7eadd13e
Pavel Djundik <xPaw@users.noreply.github.com>
2023-10-30 11:22:47 +0200

    Use Stopwatch instead of wall clock for async job timeouts

---
+++ SteamKit2/SteamKit2/Util/ValueStopwatch.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace SteamKit2.Util
+{
+    readonly struct ValueStopwatch
+    {
+        private static readonly double s_timestampToTicks = TimeSpan.TicksPerSecond / ( double )Stopwatch.Frequency;
+
+        private readonly long _startTimestamp;
+
+        private ValueStopwatch( long startTimestamp )
+        {
+            _startTimestamp = startTimestamp;
+        }
+
+        public static ValueStopwatch StartNew() => new ValueStopwatch( Stopwatch.GetTimestamp() );
+
+        public TimeSpan GetElapsedTime()
+        {
+            if ( _startTimestamp == 0 )
+            {
+                throw new InvalidOperationException( "An uninitialized, or 'default', ValueStopwatch cannot be used to get elapsed time." );
+            }
+
+            long end = Stopwatch.GetTimestamp();
+            long timestampDelta = end - _startTimestamp;
+            long ticks = ( long )( s_timestampToTicks * timestampDelta );
+            return new TimeSpan( ticks );
+        }
+    }
+}
