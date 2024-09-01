50039dd0a10c65c8d5cc570ef3f2fc480d8e6245
Tim Pilius <tpilius@gmail.com>
2024-09-01 18:59:30 -0400

    Changing default Protocol from TCP to WebSockets.  After Valve's maintenance on 2024/08/27 some servers in affected regions will no longer accept TCP connections, which can prevent clients from ever making a successful connection in some cases.  WebSockets appear to be the preferred protocol, so that should now be the new default.

--- SteamKit2/SteamKit2/Steam/SteamClient/Configuration/SteamConfigurationBuilder.cs
+++ SteamKit2/SteamKit2/Steam/SteamClient/Configuration/SteamConfigurationBuilder.cs
@@ -35,7 +35,7 @@ namespace SteamKit2
 
                 MachineInfoProvider = MachineInfoProvider.GetDefaultProvider(),
 
-                ProtocolTypes = ProtocolTypes.Tcp,
+                ProtocolTypes = ProtocolTypes.WebSocket,
 
                 ServerListProvider = new MemoryServerListProvider(),
 
