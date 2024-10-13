d9f6b1b1d38d51823ca77f8e51648cafb2ce264a
affederaffe <affederaffe@protonmail.com>
2024-10-13 11:58:04 +0200

    Use new UnifiedMessages service for SteamContent

--- SteamKit2/SteamKit2/Steam/Handlers/SteamContent/SteamContent.cs
+++ SteamKit2/SteamKit2/Steam/Handlers/SteamContent/SteamContent.cs
@@ -33,12 +33,11 @@ namespace SteamKit2
             /// </summary>
             public DateTime Expiration { get; set; }
 
-            internal CDNAuthToken( SteamUnifiedMessages.ServiceMethodResponse message )
+            internal CDNAuthToken( SteamUnifiedMessages.ServiceMethodResponse<CContentServerDirectory_GetCDNAuthToken_Response> message )
             {
-                var response = message.GetDeserializedResponse<CContentServerDirectory_GetCDNAuthToken_Response>();
                 Result = message.Result;
-                Token = response.token;
-                Expiration = DateUtils.DateTimeFromUnixTime( response.expiration_time );
+                Token = message.Body.token;
+                Expiration = DateUtils.DateTimeFromUnixTime( message.Body.expiration_time );
             }
         }
 
@@ -136,10 +135,10 @@ namespace SteamKit2
             // SendMessage is an AsyncJob, but we want to deserialize it
             // can't really do HandleMsg because it requires parsing the service like its done in HandleServiceMethod
             var unifiedMessages = Client.GetHandler<SteamUnifiedMessages>()!;
-            var contentService = unifiedMessages.CreateService<IContentServerDirectory>();
-            var message = await contentService.SendMessage(api => api.GetCDNAuthToken(request));
+            var contentService = unifiedMessages.CreateService<ContentServerDirectory>();
+            var result = await contentService.GetCDNAuthToken( request );
 
-            return new CDNAuthToken( message );
+            return new CDNAuthToken( result );
         }
 
         /// <summary>
