50b7b8b1964597bba88fd6d99fa0ddd33638bae7
Nicholas Hastings <nshastings@gmail.com>
2025-08-21 17:33:04 -0400

    NHA2: Add new Deadlock SOTypes

--- Resources/NetHookAnalyzer2/NetHookAnalyzer2/Specializations/Deadlock/DeadlockSOHelper.cs
+++ Resources/NetHookAnalyzer2/NetHookAnalyzer2/Specializations/Deadlock/DeadlockSOHelper.cs
@@ -8,8 +8,16 @@ namespace NetHookAnalyzer2.Specializations
     {
         public static Dictionary<int, Type> SOTypes = new()
         {
+			{1, typeof(CSOEconItem)},
+			{7, typeof(CSOEconGameAccountClient)},
 			{101, typeof(CSOCitadelLobby)},
+			{102, typeof(CSOCitadelServerStaticLobby)},
+			{104, typeof(CSOGameAccountClient)},
 			{105, typeof(CSOCitadelParty)},
+			{106, typeof(CSOCitadelServerDynamicLobby)},
+			{107, typeof(CSOAccountHeroInfo)},
+			{109, typeof(CSOAccountChallenge)},
+			{110, typeof(CSOCitadelHideoutLobby)},
 		};
     }
 }
