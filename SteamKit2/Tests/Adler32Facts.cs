3a7c46c92b8a64e765a8753ce8299f5fd2e8ce37
Pavel Djundik <xPaw@users.noreply.github.com>
2025-09-18 12:50:13 +0300

    Test some edge cases

--- SteamKit2/Tests/Adler32Facts.cs
+++ SteamKit2/Tests/Adler32Facts.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Text;
 using SteamKit2;
 using Xunit;
 
@@ -16,11 +17,19 @@ public class Adler32Facts
     [Theory]
     [InlineData( 0 )]
     [InlineData( 8 )]
+    [InlineData( 15 )]
+    [InlineData( 16 )]
+    [InlineData( 17 )]
     [InlineData( 215 )]
     [InlineData( 1024 )]
     [InlineData( 1024 + 15 )]
     [InlineData( 2034 )]
     [InlineData( 4096 )]
+    [InlineData( 5552 - 1 )]
+    [InlineData( 5552 )]
+    [InlineData( 5552 + 1 )]
+    [InlineData( 5552 + 16 )]
+    [InlineData( 5552 * 2 )]
     public void MatchesReference( int length )
     {
         const uint Seed = 0;
@@ -28,14 +37,37 @@ public class Adler32Facts
         var data = new byte[ length ];
         Random.Shared.NextBytes( data );
 
-        var expected = ReferenceImplementation( data );
+        var expected = ReferenceImplementation( Seed, data );
         var actual = Adler32.Calculate( Seed, data );
         Assert.Equal( expected, actual );
     }
 
-    private static uint ReferenceImplementation( ReadOnlySpan<byte> input )
+    [Theory]
+    [InlineData( 1, "a", 0x00620062 )]
+    [InlineData( 1, "Wikipedia", 0x11E60398 )]
+    [InlineData( 1, "123456789", 0x091e01de )]
+    [InlineData( 1, "SteamKit is good software, you should use it. :)", 0xaf8110e6 )]
+    public void MatchesKnownVectors( uint seed, string input, uint expected )
     {
-        uint a = 0, b = 0;
+        var data = Encoding.ASCII.GetBytes( input );
+        var actual = Adler32.Calculate( seed, data );
+        Assert.Equal( expected, actual );
+    }
+
+    [Fact]
+    public void NoOverflowWithMaxBytes()
+    {
+        var data = new byte[ 5552 ];
+        Array.Fill( data, (byte)0xFF );
+
+        var expected = ReferenceImplementation( 1, data );
+        var actual = Adler32.Calculate( 1, data );
+        Assert.Equal( expected, actual );
+    }
+
+    private static uint ReferenceImplementation( uint seed, ReadOnlySpan<byte> input )
+    {
+        uint a = seed, b = 0;
         for ( var i = 0; i < input.Length; i++ )
         {
             a = ( a + input[ i ] ) % 65521;
